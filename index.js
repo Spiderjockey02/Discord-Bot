bb342985dc04f463cc4a11aec15afe8b2dc9c708
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-09-17 18:13:40 +0100

    adding function for bot 'sharding'.

--- index.js
+++ index.js
@@ -1,71 +1,14 @@
-//Make sure Node.js V12 or higher is being ran.
-if (process.version.slice(1).split('.')[0] < 12) throw new Error('Node 12.0.0 or higher is required.');
-//Dependencies
-const Discord = require('discord.js');
-const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-const fs = require('fs');
-const path = require("path");
-const { promisify } = require("util");
-const readdir = promisify(require("fs").readdir);
+//This file is for sharding
+const { ShardingManager } = require('discord.js');
 
-//Logger (console log + file log)
-bot.logger = require("./modules/logger");
-//For command handler
-bot.aliases = new Discord.Collection();
-bot.commands = new Discord.Collection();
+const manager = new ShardingManager('./bot.js', {
+  //Sharding options
+  totalShards: 'auto',
+  token: 'NjQ3MjAzOTQyOTAzODQwNzc5.XdcTAw.gq5ffGBb2b1sKdjhB_bxo2ZuYH0'
+});
 
-//Get Bot commands
-const Cmodules = [`Fun`, `Guild`, 'Host', 'Levels', 'Music', `Search`, `Trivia`, `Misc`]
-//Load commands
-Cmodules.forEach(c => {
-	fs.readdir(`./commands/${c}`, (err, files) => {
-		if (err) console.log(err)
-		files.forEach(f => {
-			const cmds = require(`./commands/${c}/${f}`);
-			bot.logger.log(`Loading command: ${f}`)
-			bot.commands.set(cmds.config.command, cmds);
-			cmds.config.aliases.forEach(alias => {
-				bot.aliases.set(alias, cmds.config.command)
-			})
-		})
-	})
-})
+// Spawn your shards
+manager.spawn();
 
-//connect to database and get global functions
-bot.mongoose = require('./modules/mongoose')
-require('./modules/function.js')(bot)
-
-//Load config for Egglord
-try {
-	bot.config = require('./config.js');
-} catch (err) {
-	console.error('Unable to load config.js \n', err);
-	process.exit(1);
-}
-
-//Console chatter (add - allow commands to run from console)
-let y = process.openStdin()
-y.addListener("data", res => {
-	let message = res.toString().trim()
-	let args = res.toString().trim().split(/ +/g)
-	//now run command
-	if (args.length == 0) return
-	require('./Utils/console.js').run(args, message, bot)
-})
-
-//Load events (what the bot does)
-const init = async () => {
-	await require("./modules/eventHandler")(bot)
-	//Get web dashboard if enabled
-	if (bot.config.Dashboard.enabled == true) {
-		require("./modules/dashboard")(bot)
-	}
-	//Connect bot to database
-	bot.mongoose.init(bot)
-	//Connect bot to discord API
-	var token = bot.config.token;
-	bot.login(token).catch(e => bot.logger.log(e.message,"error"))
-}
-
-//Bot logins in
-init();
+// Emitted when a shard is created
+manager.on('shardCreate', (shard) => require("./modules/logger").log(`Shard ${shard.id} launched`));
