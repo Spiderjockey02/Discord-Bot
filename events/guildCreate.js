d43eea54dbfccb83dd667f9ef141892617344d72
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-09-18 20:55:25 +0100

    implemented 'discord.boats' API

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,6 @@
 //When the bot joins a server
+const BOATS = require('boats.js');
+
 module.exports = async (bot, guild) => {
   //LOG server Join
   bot.logger.log(`[GUILD JOIN] ${guild.name} (${guild.id}) added the bot.`);
@@ -13,4 +15,8 @@ module.exports = async (bot, guild) => {
   } catch (e) {
     console.error(e)
   }
+
+  //update Discord.boats website
+  const Boats = new BOATS(bot.config.DiscordBoatAPI_Key);
+  await Boats.postStats(bot.guilds.cache.size, '647203942903840779');
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,12 @@
 //When the bot leaves a guild
+const BOATS = require('boats.js');
+
 module.exports = async (bot, guild) => {
   bot.logger.cmd(`[GUILD LEAVE] ${guild.name} (${guild.id}) removed the bot.`, 'log');
   //Delete server settings
   await bot.DeleteGuild(guild)
+
+  //update Discord.boats website
+  const Boats = new BOATS(bot.config.DiscordBoatAPI_Key);
+  await Boats.postStats(bot.guilds.cache.size, '647203942903840779');
 };
