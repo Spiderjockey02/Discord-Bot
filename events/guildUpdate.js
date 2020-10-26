8a46bb8506e965c478190a114548b53b28028927
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-10-26 11:26:05 +0000

    bug fixes and added database update on guild name change.

--- events/guildUpdate.js
+++ events/guildUpdate.js
@@ -11,35 +11,36 @@ module.exports = async (bot, oldGuild, newGuild) => {
 	// Get server settings
 	let settings;
 	try {
-		settings = await bot.getGuild(newGuild.guild);
+		settings = await bot.getGuild(newGuild);
 	} catch (e) {
 		console.log(e);
 	}
-	// Check if ModLog plugin is active
-	if (settings.ModLog == false) return;
-	// Check if event channelDelete is for logging
-	if (settings.ModLogEvents.includes('GUILDUPDATE')) {
-		// guild name change
-		if (oldGuild.name != newGuild.name) {
-			const embed = new Discord.MessageEmbed()
-				.setDescription('**Server name changed**')
-				.setAuthor(newGuild.name, newGuild.iconURL())
-				.addField('Before:', oldGuild.name)
-				.addField('After:', newGuild.name)
-				.setTimestamp();
-			sendMessage(newGuild, settings, embed);
-		}
-		// region change
-		if (oldGuild.region != newGuild.region) {
-			const embed = new Discord.MessageEmbed()
-				.setDescription('**Server region changed**')
-				.setAuthor(newGuild.name, newGuild.iconURL())
-				.addField('Before:', oldGuild.region)
-				.addField('After:', newGuild.region)
-				.setTimestamp();
-			sendMessage(newGuild, settings, embed);
-		}
+
+	// Check for updates
+	let embed;
+	if (oldGuild.name != newGuild.name) {
+		embed = new Discord.MessageEmbed()
+			.setDescription('**Server name changed**')
+			.setAuthor(newGuild.name, newGuild.iconURL())
+			.addField('Before:', oldGuild.name)
+			.addField('After:', newGuild.name)
+			.setTimestamp();
+		bot.updateGuild(newGuild, { guildName: newGuild.name });
 	}
+	// region change
+	if (oldGuild.region != newGuild.region) {
+		embed = new Discord.MessageEmbed()
+			.setDescription('**Server region changed**')
+			.setAuthor(newGuild.name, newGuild.iconURL())
+			.addField('Before:', oldGuild.region)
+			.addField('After:', newGuild.region)
+			.setTimestamp();
+	}
+	// Check if event channelDelete is for logging
+	if (settings.ModLog & settings.ModLogEvents.includes('GUILDUPDATE')) {
+		sendMessage(newGuild, settings, embed);
+	}
+
 	// log event in console
 	bot.logger.log(`Guild: ${newGuild.name} has been updated.`);
 };
