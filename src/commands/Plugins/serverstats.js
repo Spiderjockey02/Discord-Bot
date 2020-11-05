0a67795f19a503c9aa2009ede65e5ca9ec894171
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-11-05 22:43:38 +0000

    added server stats

---
+++ src/commands/Plugins/serverstats.js
@@ -0,0 +1,52 @@
+module.exports.run = async (bot, message, args, emojis, settings) => {
+	// Delete message
+	if (settings.ModerationClearToggle & message.deletable) message.delete();
+
+	// Make sure user can edit server plugins
+	if (!message.member.hasPermission('MANAGE_GUILD')) {
+		message.channel.send({ embed:{ color:15158332, description:`${emojis[0]} You are missing the permission: \`MANAGE_GUILD\`.` } }).then(m => m.delete({ timeout: 10000 }));
+		return;
+	}
+
+	// Check to see if user dosesn't know how to use command or not
+	if (!args[0] | args[0] == '?') return message.channel.send({ embed:{ color:15158332, description:`${emojis[0]} Please use the format \`${bot.commands.get('serverstats').help.usage.replace('${PREFIX}', settings.prefix)}\`.` } }).then(m => m.delete({ timeout: 5000 }));
+
+
+	if (args[0] == 'on') {
+		await message.guild.channels.create('📊 Server Stats 📊', { type: 'category', reason: 'Server stats set-up' }).then(async channel => {
+			// add bot channel
+			const botChannel = await message.guild.channels.create(`Bot count: ${message.guild.members.cache.filter(m => m.user.bot).size}`, { type: 'voice' });
+			botChannel.setParent(channel.id);
+			// add user channel
+			const userChannel = await message.guild.channels.create(`User count: ${message.guild.members.cache.size}`, { type: 'voice' });
+			userChannel.setParent(channel.id);
+			// add human channel
+			const humanChannel = await message.guild.channels.create(`Human count: ${message.guild.members.cache.filter(m => !m.user.bot).size}`, { type: 'voice' });
+			humanChannel.setParent(channel.id);
+			bot.updateGuild(message.guild, { ServerStats: true, ServerStatsCate: channel.id, ServerStatsBot: true, ServerStatsBotChannel: botChannel.id, ServerStatsUser: true, ServerStatsUserChannel: userChannel.id, ServerStatsHuman: true, ServerStatsHumanChannel: humanChannel.id });
+		});
+	} else {
+		const botChannel = message.guild.channels.cache.find(c => c.id == settings.ServerStatsBotChannel);
+		if (botChannel) botChannel.delete();
+		const userChannel = message.guild.channels.cache.find(c => c.id == settings.ServerStatsUserChannel);
+		if (userChannel) userChannel.delete();
+		const humanChannel = message.guild.channels.cache.find(c => c.id == settings.ServerStatsHumanChannel);
+		if (humanChannel) humanChannel.delete();
+		const cateChannel = message.guild.channels.cache.find(c => c.id == settings.ServerStatsCate);
+		if (cateChannel) cateChannel.delete();
+		bot.updateGuild(message.guild, { ServerStats: false, ServerStatsCate: '00', ServerStatsBot: false, ServerStatsBotChannel: '00', ServerStatsUser: false, ServerStatsUserChannel: '00', ServerStatsHuman: false, ServerStatsHumanChannel: '00' });
+	}
+};
+
+module.exports.config = {
+	command: 'serverstats',
+	aliases: ['server-stats'],
+	permissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
+};
+
+module.exports.help = {
+	name: 'serverstats',
+	category: 'Plugins',
+	description: 'Turn on or off the serverstats.',
+	usage: '${PREFIX}serverstats <on | off>',
+};
--- src/events/guildMemberRemove.js
+++ src/events/guildMemberRemove.js
@@ -11,6 +11,32 @@ module.exports = async (bot, member) => {
 	} catch (e) {
 		console.log(e);
 	}
+
+	// serverstats
+	if (settings.ServerStats) {
+		const membercache = member.guild.members.cache;
+		// update bot stats
+		if (settings.ServerStatsBot) {
+			const botChannel = member.guild.channels.cache.find(c => c.id == settings.ServerStatsBotChannel);
+			if (botChannel) {
+				await botChannel.setName(`Total bots: ${membercache.filter(m => m.user.bot).size}`).catch(e => console.log(e));
+			}
+		}
+		// update user stats (total member count)
+		if (settings.ServerStatsUser) {
+			const userChannel = member.guild.channels.cache.find(c => c.id == settings.ServerStatsUserChannel);
+			if (userChannel) {
+				await userChannel.setName(`Total users: ${membercache.size}`).catch(e => console.log(e));
+			}
+		}
+		if (settings.ServerStatsHuman) {
+			const humanChannel = member.guild.channels.cache.find(c => c.id == settings.ServerStatsHumanChannel);
+			if (humanChannel) {
+				await humanChannel.setName(`Total humans: ${membercache.filter(m => !m.user.bot).size}`).catch(e => console.log(e));
+			}
+		}
+	}
+
 	// Logging plugin
 	if (settings.ModLog == false) return;
 	// Check if event guildMemberAdd is for logging
