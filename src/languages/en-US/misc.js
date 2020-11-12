a9dbba1126e3cfe2c431151cfb10ce37289b13f4
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-11-12 20:43:29 +0000

    Updated misc translation

--- src/languages/en-US/misc.js
+++ src/languages/en-US/misc.js
@@ -1,13 +1,10 @@
 const languageData = {
-	// Miscellenous
 	ERROR_MESSAGE: 'An error occured when running this command, please try again or contact support.',
 	INCORRECT_FORMAT: (commandExample) => `Please use the format: \`${commandExample}\`.`,
 	MISSING_PERMISSION: (permission) => `I am missing the permission: \`${permission}\`.`,
 	USER_PERMISSION: (permission) => `You are missing the permission: \`${permission}\`.`,
 	MISSING_ROLE: 'I was unable to find this role.',
 	NO_REASON: 'No reason given.',
-	// Level
-	// Misc
 };
 
 const translate = (key, args) => {
