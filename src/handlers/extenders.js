19bacc9ddeccced2d123d6b8832044eda42758c4
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-11-14 00:35:31 +0000

    Updated extender to allow commands in DM's

--- src/handlers/extenders.js
+++ src/handlers/extenders.js
@@ -18,24 +18,38 @@ module.exports = async (bot) => {
 	Message.prototype.sendT = function(language, key, args) {
 		try {
 			return this.channel.send(this.translate(language, key, args));
-		} catch (e) {
-			console.log(e);
+		} catch (err) {
+			bot.logger.error(err.message);
 		}
 	};
 
 	// This will add the error emoji as the prefix and then translate the message
 	Message.prototype.error = function(language, key, args) {
 		try {
-			const emoji = this.channel.permissionsFor(bot.user).has('USE_EXTERNAL_EMOJIS') ? bot.config.emojis.cross : ':negative_squared_cross_mark:';
+			let emoji;
+			if (this.channel.type == 'dm') {
+				emoji = bot.config.emojis.cross;
+			} else {
+				emoji = this.channel.permissionsFor(bot.user).has('USE_EXTERNAL_EMOJIS') ? bot.config.emojis.cross : ':negative_squared_cross_mark:';
+			}
 			return this.channel.send({ embed:{ color:15158332, description:`${emoji} ${this.translate(language, key, args)}` } });
-		} catch (e) {
-			console.log(e);
+		} catch (err) {
+			bot.logger.error(err.message);
 		}
 	};
 
 	// This will add the success emoji as the prefix and then translate the message
 	Message.prototype.success = function(language, key, args) {
-		const emoji = (this.channel.permissionsFor(bot.user).has('USE_EXTERNAL_EMOJIS')) ? bot.config.emojis.tick : ':white_check_mark:';
-		return this.channel.send({ embed:{ color:3066993, description:`${emoji} ${this.translate(language, key, args)}` } });
+		try {
+			let emoji;
+			if (this.channel.type == 'dm') {
+				emoji = bot.config.emojis.tick;
+			} else {
+				emoji = this.channel.permissionsFor(bot.user).has('USE_EXTERNAL_EMOJIS') ? bot.config.emojis.tick : ':white_check_mark:';
+			}
+			return this.channel.send({ embed:{ color:3066993, description:`${emoji} ${this.translate(language, key, args)}` } });
+		} catch (err) {
+			bot.logger.error(err.message);
+		}
 	};
 };
