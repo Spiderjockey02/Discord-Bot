d06c882562a19e8616074e4139e78734aea96f63
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-11-18 23:00:26 +0000

    Updated giveaway start command

--- src/commands/Giveaway/g-start.js
+++ src/commands/Giveaway/g-start.js
@@ -2,6 +2,12 @@ module.exports.run = async (bot, message, args, settings) => {
 	// Make sure the user has the right permissions to use giveaway
 	if (!message.member.hasPermission('ADMINISTRATOR')) return message.error(settings.Language, 'USER_PERMISSION', 'ADMINISTRATOR').then(m => m.delete({ timeout: 10000 }));
 
+	// Make sure a time, winner count & prize is entered
+	if (!args[0] || !args[1] || !args[2]) {
+		if (message.deletable) message.delete();
+		return message.error(settings.Language, 'INCORRECT_FORMAT', bot.commands.get('g-start').help.usage.replace('${PREFIX}', settings.prefix)).then(m => m.delete({ timeout: 5000 }));
+	}
+
 	// Get time
 	const time = require('../../helpers/time-converter.js').getTotalTime(args[0], message, settings.Language);
 	if (!time) return;
@@ -12,19 +18,13 @@ module.exports.run = async (bot, message, args, settings) => {
 		return message.error(settings.Language, 'GIVEAWAY/INCORRECT_WINNER_COUNT').then(m => m.delete({ timeout: 5000 }));
 	}
 
-	// Check for prize
-	if (!args[2]) {
-		if (message.deletable) message.delete();
-		return message.error(settings.Language, 'INCORRECT_FORMAT', bot.commands.get('g-start').help.usage.replace('${PREFIX}', settings.prefix)).then(m => m.delete({ timeout: 5000 }));
-	}
-
 	// Start the giveaway
 	bot.giveawaysManager.start(message.channel, {
 		time: time,
 		prize: args.slice(2).join(' '),
 		winnerCount: parseInt(args[1]),
-	}).then((gData) => {
-		console.log(gData);
+	}).then(() => {
+		bot.logger.log(`${message.author.tag} started a giveaway in server: [${message.guild.id}].`);
 	});
 };
 
