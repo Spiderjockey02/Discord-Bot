6582e428d1a286b386be781f5c0c02627cb0852a
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-11-23 20:32:10 +0000

    Removed discord bot list updated (has been moved)

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,8 +1,3 @@
-// Dependencies
-const BOATS = require('boats.js');
-const { post } = require('axios');
-const BotList = require('botlist.space');
-
 // When the bot joins a server
 module.exports = async (bot, guild) => {
 	// LOG server Join
@@ -17,33 +12,5 @@ module.exports = async (bot, guild) => {
 		await bot.CreateGuild(newGuild);
 	} catch (e) {
 		console.error(e);
-	}
-	// Check to see if bot is connected to any Discord bot websites
-	if (bot.config.DiscordBotLists) {
-		// Check if bot is connected to https://discord.boats/
-		if (bot.config.DiscordBotLists.DiscordBoatAPI_Key) {
-			// update Discord.boats website
-			const Boats = new BOATS(bot.config.DiscordBoatAPI_Key);
-			await Boats.postStats(bot.guilds.cache.size, '647203942903840779');
-		}
-		// update https://arcane-center.xyz/ bot stats
-		if (bot.config.DiscordBotLists.ArcaneBotAPI_KEY) {
-			// update https://arcane-center.xyz/ bot stats
-			post(`https://arcane-center.xyz/api/${bot.user.id}/stats`, { server_count: bot.guilds.cache.size, shard_count: bot.options.shardCount }, {
-				headers: {
-					'Content-Type': 'application/json',
-					'Authorization': require('../config.js').ArcaneBotAPI_KEY,
-				},
-			}).then(res => {
-				console.log(res);
-			}).catch(err => {
-				console.error(err);
-			});
-		}
-		// update https://botlist.space/ bot stats
-		if (bot.config.DiscordBotLists.botlist_spaceAPI_KEY) {
-			const client = new BotList.Client({ id: bot.user.id, botToken: require('../config.js').botlist_spaceAPI_KEY });
-			await client.postServerCount(bot.guilds.cache.size).catch(e => console.log(e));
-		}
 	}
 };
--- src/events/guildDelete.js
+++ src/events/guildDelete.js
@@ -1,12 +1,6 @@
 // When the bot leaves a guild
-const BOATS = require('boats.js');
-
 module.exports = async (bot, guild) => {
 	bot.logger.log(`[GUILD LEAVE] ${guild.name} (${guild.id}) removed the bot.`);
 	// Delete server settings
 	await bot.DeleteGuild(guild);
-
-	// update Discord.boats website
-	const Boats = new BOATS(bot.config.DiscordBoatAPI_Key);
-	await Boats.postStats(bot.guilds.cache.size, '647203942903840779');
 };
