89a8274c5bdd888f05d37637e466981215ea68cf
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-11-27 20:27:33 +0000

    updated spotify handler

--- src/base/Music/SpotifyHandler.js
+++ src/base/Music/SpotifyHandler.js
@@ -1,9 +1,25 @@
 'use strict';
 const __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
-	function adopt(value) { return value instanceof P ? value : new P(function(resolve) { resolve(value); }); }
+	function adopt(value) {
+		return value instanceof P ? value : new P(function(resolve) {
+			resolve(value);
+		});
+	}
 	return new (P || (P = Promise))(function(resolve, reject) {
-		function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-		function rejected(value) { try { step(generator['throw'](value)); } catch (e) { reject(e); } }
+		function fulfilled(value) {
+			try {
+				step(generator.next(value));
+			} catch (e) {
+				reject(e);
+			}
+		}
+		function rejected(value) {
+			try {
+				step(generator['throw'](value));
+			} catch (e) {
+				reject(e);
+			}
+		}
 		function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
 		step((generator = generator.apply(thisArg, _arguments || [])).next());
 	});
@@ -16,7 +32,7 @@ exports.Spotify = void 0;
 const erela_js_1 = require('erela.js');
 const axios_1 = __importDefault(require('axios'));
 const BASE_URL = 'https://api.spotify.com/v1';
-const REGEX = /(?:https:\/\/open\.spotify\.com\/|spotify:)(.+)(?:[\/:])([A-Za-z0-9]+)/;
+const REGEX = /(?:https:\/\/open\.spotify\.com\/|spotify:)(.+)(?:[/:])([A-Za-z0-9]+)/;
 const buildSearch = (loadType, tracks, error, name) => ({
 	loadType: loadType,
 	tracks: tracks !== null && tracks !== void 0 ? tracks : [],
