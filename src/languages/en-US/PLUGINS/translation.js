1f523be9a7164e1a7a17cbd8ea7b10d34b9e6dd5
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-11-30 09:24:42 +0000

    added plugins translations

--- src/languages/en-US/PLUGINS/translation.js
+++ src/languages/en-US/PLUGINS/translation.js
@@ -6,14 +6,17 @@ const languageData = {
 	MUSIC_SET: (option) => `Music plugin has been set to \`${option}\`.`,
 	NSFW_SET: (option) => `NSFW plugin has been set to \`${option}\`.`,
 	SEARCH_SET: (option) => `Search plugin has been set to \`${option}\`.`,
-
+	LOGS_SET: (option) => `Logging plugin has been set to \`${option}\`.`,
+	// error message
 	MISSING_LANGUAGE: 'No language selected.',
 	NO_LANGUAGE: 'That is not a language I support, yet. Why not help me learn that language by joining here: https://discord.gg/8g6zUQu',
+	// other success messages
+	LOG_CHANNEL: (channelID) => `Logging channel updated to: <#${channelID}>`,
 };
 
 const translate = (key, args) => {
 	const translation = languageData[key];
-	if(typeof translation === 'function') return translation(args);
+	if (typeof translation === 'function') return translation(args);
 	else return translation;
 };
 
