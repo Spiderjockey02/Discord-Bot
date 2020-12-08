65ef08243a6e58af6cce79fff578434d826d2359
Spiderjockey02 <benjamin.forey11@gmail.com>
2020-12-08 18:34:30 +0000

    updated image command

--- src/commands/Image/image.js
+++ src/commands/Image/image.js
@@ -1,56 +1,27 @@
 // Dependencies
-const { load } = require('cheerio');
-const request = require('request');
+const { image_search } = require('duckduckgo-images-api');
+const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (bot, message, args, settings) => {
 	// Make sure a topic was included
 	if (!args[0]) {
-		try {
-			if (message.deletable) message.delete();
-			return message.error(settings.Language, 'INCORRECT_FORMAT', bot.commands.get('img').help.usage.replace('${PREFIX}', settings.prefix)).then(m => m.delete({ timeout: 5000 }));
-		} catch (e) {
-			console.log(e);
-		}
+		if (message.deletable) message.delete();
+		return message.error(settings.Language, 'INCORRECT_FORMAT', bot.commands.get('img').help.usage.replace('${PREFIX}', settings.prefix)).then(m => m.delete({ timeout: 5000 }));
 	}
-	const word = args.join(' ');
-	// send 'waiting' message
-	const msg = await message.sendT(settings.Language, 'IMAGE/GENERATING_IMAGE');
 
-	// Option for searching for image
-	const options = {
-		url: 'http://results.dogpile.com/serp?qc=images&q=' + `${word}`,
-		method: 'GET',
-		headers: {
-			'Accept': 'text/html',
-			'User-Agent': 'Chrome',
-		},
-	};
+	let results;
+	if (message.channel.nsfw || message.channel.type == 'dm') {
+		// NSFW content can be shown
+		results = await image_search({ query: args.join(' '), moderate: false, iterations: 2, retries: 2 });
+	} else {
+		// NSFW can't be shown
+		results = await image_search({ query: args.join(' '), moderate: true, iterations: 2, retries: 2 });
+	}
 
-	// search for image
-	request(options, function(error, response, responseBody) {
-		// if an error occured
-		if (error) {
-			if (message.deletable) message.delete();
-			msg.delete();
-			message.error(settings.Language, 'ERROR_MESSAGE').then(m => m.delete({ timeout: 5000 }));
-			return;
-		}
-		// Retrieve image(s)
-		const $ = load(responseBody);
-		const links = $('.image a.link');
-		const urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr('href'));
-		// If no images found
-		if (!urls.length) {
-			if (message.deletable) message.delete();
-			msg.delete();
-			message.error(settings.Language, 'ERROR_MESSAGE').then(m => m.delete({ timeout: 5000 }));
-			return;
-		}
-		// Displays image in channel
-		msg.delete();
-		message.channel.send({ embed:{ image:{ url:`${urls[Math.floor(Math.random() * urls.length)]}` } } });
-	});
-
+	// send image
+	const embed = new MessageEmbed()
+		.setImage(results[Math.floor(Math.random() * 101)].image);
+	message.channel.send(embed);
 };
 
 module.exports.config = {
