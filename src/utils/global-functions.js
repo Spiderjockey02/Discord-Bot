e47520830700060163f142046845a78f4be909db
Spiderjockey2 <benjamin.forey11gmail.com>
2021-01-08 12:01:25 +0000

    Converted GIF avatars to PNG for image commands

--- src/utils/global-functions.js
+++ src/utils/global-functions.js
@@ -73,14 +73,14 @@ module.exports = bot => {
 		} else {
 			// check user
 			if (user != message.author) {
-				file.push(user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }));
+				file.push(user.displayAvatarURL({ format: 'png', size: 1024 }));
 			}
 			// Checks if a link to image was entered
 			if (args[1] && !(args[1].startsWith('<') && args[1].endsWith('>'))) {
 				file.push(args[1]);
 			}
 			// add user
-			file.push(message.author.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }));
+			file.push(message.author.displayAvatarURL({ format: 'png', size: 1024 }));
 			// send file;
 		}
 		return file;
