d2273a1fb0d705bb7d8fc9c55600307f1ff0c6fe
Spiderjockey2 <benjamin.forey11gmail.com>
2021-01-10 20:26:09 +0000

    updated server-info command

--- src/commands/Guild/server-info.js
+++ src/commands/Guild/server-info.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 module.exports.run = async (bot, message, args, settings) => {
 	// Send server information
 	const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).array();
-	while (roles.join(', ').length >= 253) {
+	while (roles.join(', ').length >= 1021) {
 		roles.pop();
 	}
 	const member = message.guild.members.cache;
@@ -21,7 +21,7 @@ module.exports.run = async (bot, message, args, settings) => {
 		.addField(message.translate(settings.Language, 'GUILD/GUILD_VERIFICATION'), `\`${message.guild.verificationLevel}\``, true)
 		.addField(message.translate(settings.Language, 'GUILD/GUILD_MEMBER', message.guild.memberCount), `\`${(member.filter(m => m.presence.status === 'online').size)} online, ${(member.filter(m => m.presence.status === 'idle').size)} idle and ${(member.filter(m => m.presence.status === 'dnd').size)} DnD \n${member.filter(m => m.user.bot).size} bots, ${member.filter(m => !m.user.bot).size} humans\``, true)
 		.addField(message.translate(settings.Language, 'GUILD/GUILD_FEATURES'), `\`${(message.guild.features.length == 0) ? 'None' : message.guild.features.toString().toLowerCase().replace(/,/g, ', ')}\``, true)
-		.addField(message.translate(settings.Language, 'GUILD/GUILD_ROLES', message.guild.roles.cache.size), `${roles.join(', ')}...`)
+		.addField(message.translate(settings.Language, 'GUILD/GUILD_ROLES', message.guild.roles.cache.size), `${roles.join(', ')}${(roles.length != message.guild.roles.cache.sort((a, b) => b.position - a.position).array().length) ? '...' : '.'}`)
 		.setTimestamp()
 		.setFooter(message.translate(settings.Language, 'GUILD/INFO_FOOTER', message.author.tag));
 	message.channel.send(embed);
