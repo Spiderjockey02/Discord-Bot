ddb82d0b4b3171201ad4533b334e049bc4b9be49
Spiderjockey2 <benjamin.forey11gmail.com>
2021-01-24 17:32:35 +0000

    removed file logger as it caused nothing but problems.

--- src/modules/logging/logger.js
+++ src/modules/logging/logger.js
@@ -1,47 +1,27 @@
 // Dependecies
 const chalk = require('chalk');
 const moment = require('moment');
-const opts = {
-	logDirectory:'./src/modules/logging/logs',
-	fileNamePattern:'roll-<DATE>.log',
-	dateFormat:'YYYY.MM.DD',
-};
 exports.log = (content, type = 'log') => {
 	if (content == 'error') return;
 	const timestamp = `[${moment().format('HH:mm:ss')}]:`;
-	const log = require('simple-node-logger').createRollingFileLogger(opts);
-	switch (type) {
-	case 'log': {
+	if (type == 'log') {
 		console.log(`${timestamp} ${chalk.bgBlue(type.toUpperCase())} ${content} `);
-		log.info(content);
 		return;
-	}
-	case 'warn': {
+	} else if (type == 'warn') {
 		console.log(`${timestamp} ${chalk.black.bgYellow(type.toUpperCase())} ${content} `);
-		log.warn(content);
 		return;
-	}
-	case 'error': {
+	} else if (type == 'error') {
 		console.log(`${timestamp} ${chalk.bgRed(type.toUpperCase())} ${content} `);
-		log.error(content);
 		return;
-	}
-	case 'debug': {
+	} else if (type == 'debug') {
 		console.log(`${timestamp} ${chalk.green(type.toUpperCase())} ${content} `);
-		log.debug(content);
 		return;
-	}
-	case 'cmd': {
+	} else if (type == 'cmd') {
 		console.log(`${timestamp} ${chalk.black.bgWhite(type.toUpperCase())} ${content}`);
-		log.info(content);
 		return;
-	}
-	case 'ready': {
+	} else if (type == 'ready') {
 		console.log(`${timestamp} ${chalk.black.bgGreen(type.toUpperCase())} ${content}`);
-		log.info(content);
 		return;
-	}
-	default: throw new TypeError('Logger type must be either warn, debug, log, ready, cmd or error.');
 	}
 };
 
