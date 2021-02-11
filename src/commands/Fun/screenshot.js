2ec68b6441ff32e214158193f70a53640171cdca
Spiderjockey2 <benjamin.forey11gmail.com>
2021-02-11 22:49:44 +0000

    updated screenshot command

--- src/commands/Fun/screenshot.js
+++ src/commands/Fun/screenshot.js
@@ -26,42 +26,16 @@ module.exports.run = async (bot, message, args, settings) => {
 
 	// screenshot function
 	async function screenshot(url) {
-		const browser = await Puppeteer.launch({});
+		const browser = await Puppeteer.launch();
 		const page = await browser.newPage();
 		await page.setViewport({
 			width: 1280,
-			height: 800,
+			height: 720,
 		});
-
-		if (!url) {
-			if (!this.rawHTML) {
-				await browser.close();
-				console.log('No html content found, Please load HTML before using screenshot method!');
-				return false;
-			}
-			await page.setContent(this.rawHTML);
-			const buffer = await page.screenshot();
-			await browser.close();
-			return buffer instanceof Buffer ? buffer : Buffer.from(buffer);
-		} else {
-			if (typeof url !== 'string') {
-				await browser.close();
-				console.log(`URL type must be a string, received ${typeof url}`);
-				return false;
-			}
-			try {
-				await page.goto(url);
-			} catch (e) {
-				try {
-					await page.goto('https://' + url);
-				} catch (err) {
-					return null;
-				}
-			}
-			const buffer = await page.screenshot();
-			await browser.close();
-			return buffer instanceof Buffer ? buffer : Buffer.from(buffer);
-		}
+		await page.goto(url);
+		const r = await page.screenshot();
+		await browser.close();
+		return r;
 	}
 };
 
