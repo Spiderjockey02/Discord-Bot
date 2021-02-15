d907d6b0f963193b710b34cce3072c5a5b146f58
Spiderjockey2 <benjamin.forey11gmail.com>
2021-02-15 14:01:26 +0000

    added voiceStateUpdate for mod logging

---
+++ src/events/voiceStateUpdate.js
@@ -0,0 +1,55 @@
+// Dependencies
+const { MessageEmbed } = require('discord.js');
+
+module.exports = async (bot, oldState, newState) => {
+	// variables for easier coding
+	const newMember = newState.guild.member(newState.id);
+	const channel = newState.guild.channels.cache.get(newState.channelID);
+
+	// get server settings
+	let settings;
+	try {
+		settings = await bot.getGuild(newState.guild);
+	} catch (e) {
+		console.log(e);
+	}
+
+	// Check if event voiceStateUpdate is for logging
+	if (settings.ModLogEvents.includes('VOICESTATEUPDATE') && settings.ModLog) {
+		// member has been server (un)deafened
+		if (oldState.serverDeaf != newState.serverDeaf) {
+			const embed = new MessageEmbed()
+				.setDescription(`**${newMember} was server ${newState.serverDeaf ? '' : 'un'}deafened in ${channel.toString()}**`)
+				.setColor(newState.serverDeaf ? 15158332 : 3066993)
+				.setTimestamp()
+				.setFooter(`User: ${newMember.id}`)
+				.setAuthor(newMember.user.username, newMember.user.displayAvatarURL);
+			const modChannel = newState.guild.channels.cache.get(settings.ModLogChannel);
+			if (modChannel) modChannel.send(embed);
+		}
+
+		// member has been server (un)muted
+		if (oldState.serverMute != newState.serverMute) {
+			const embed = new MessageEmbed()
+				.setDescription(`**${newMember} was server ${newState.serverMute ? '' : 'un'}muted in ${channel.toString()}**`)
+				.setColor(newState.serverMute ? 15158332 : 3066993)
+				.setTimestamp()
+				.setFooter(`User: ${newMember.id}`)
+				.setAuthor(newMember.user.username, newMember.user.displayAvatarURL);
+			const modChannel = newState.guild.channels.cache.get(settings.ModLogChannel);
+			if (modChannel) modChannel.send(embed);
+		}
+
+		// member has (stopped/started) streaming
+		if (oldState.streaming != newState.streaming) {
+			const embed = new MessageEmbed()
+				.setDescription(`**${newMember} has ${newState.streaming ? 'started' : 'stopped'} streaming in ${channel.toString()}**`)
+				.setColor(newState.streaming ? 15158332 : 3066993)
+				.setTimestamp()
+				.setFooter(`User: ${newMember.id}`)
+				.setAuthor(newMember.user.username, newMember.user.displayAvatarURL);
+			const modChannel = newState.guild.channels.cache.get(settings.ModLogChannel);
+			if (modChannel) modChannel.send(embed);
+		}
+	}
+};
