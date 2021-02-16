0865ca7a41251512a00af365eae4096fb10b50ff
Spiderjockey2 <benjamin.forey11gmail.com>
2021-02-16 15:01:10 +0000

    updated advice command

--- src/commands/Fun/advice.js
+++ src/commands/Fun/advice.js
@@ -1,11 +1,10 @@
 // Dependencies
-const { Random } = require('something-random-on-discord');
-const random = new Random();
+const fetch = require('node-fetch');
 
 module.exports.run = async (bot, message, args, settings) => {
 	try {
-		const data = await random.getAdvice();
-		message.channel.send(data);
+		const data = await fetch('https://api.adviceslip.com/advice').then(res => res.json());
+		message.channel.send({ embed: { color: 'RANDOM', description: data.slip.advice } });
 	} catch (err) {
 		if (bot.config.debug) bot.logger.error(`${err.message} - command: advice.`);
 		message.error(settings.Language, 'ERROR_MESSAGE').then(m => m.delete({ timeout: 5000 }));
