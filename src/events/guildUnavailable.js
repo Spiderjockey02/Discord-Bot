95e68be3333884a40a56dba8fceb698f1fe02456
Spiderjockey2 <benjamin.forey11gmail.com>
2021-02-18 19:53:59 +0000

    fixed spam on event in console

--- src/events/guildUnavailable.js
+++ src/events/guildUnavailable.js
@@ -1,5 +1,16 @@
+// Variables
+const unavailableGuilds = [];
+
 // when a server has become unavailable
 module.exports = async (bot, guild) => {
-	// LOG error event
-	bot.logger.log(`[GUILD UNAVAILABLE] ${guild.name} (${guild.id}).`);
+	// only show error once an hour
+	if (unavailableGuilds.includes(guild.id)) {
+		// remove guild from array after an error
+		setTimeout(function() {
+			unavailableGuilds.splice(unavailableGuilds.indexOf(guild.id), 1);
+		}, 3600000);
+	} else {
+		bot.logger.log(`[GUILD UNAVAILABLE] ${guild.name} (${guild.id}).`);
+		unavailableGuilds.push(guild.id);
+	}
 };
