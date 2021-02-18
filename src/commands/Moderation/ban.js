d03bdd16aa18ea86344ca5214fe78d27f927447c
Spiderjockey2 <benjamin.forey11gmail.com>
2021-02-18 15:59:15 +0000

    fixed ban message reason

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -36,7 +36,7 @@ module.exports.run = async (bot, message, args, settings) => {
 				.setThumbnail(message.guild.iconURL())
 				.setDescription(`You have been banned from ${message.guild.name}.`)
 				.addField('Banned by:', message.author.tag, true)
-				.addField('Reason:', 'UNPOG', true);
+				.addField('Reason:', reason, true);
 			await member[0].send(embed);
 			// eslint-disable-next-line no-empty
 		} catch (e) {}
