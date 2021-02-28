158ad1938b4439abdd84d987d0d8529ed91016ce
Spiderjockey2 <benjamin.forey11gmail.com>
2021-02-28 21:34:34 +0000

    added music translations

--- src/languages/en-US/MUSIC/translation.js
+++ src/languages/en-US/MUSIC/translation.js
@@ -4,11 +4,14 @@ const languageData = {
 	NOT_VOICE: 'You\'re not in the same voice channel as me.',
 	MISSING_VOICE: 'You\'re not in a voice channel that I can connect to.',
 	ERROR: (error) => `There was an error while searching: \`${error}\``,
+	LIVESTREAM: 'You can\'t do that to a livestream.',
+	MISSING_DJROLE: 'You are missing the DJ role.',
 	// incorrect enteries
 	LEFT_VOICE: 'I have successfully left the voice channel.',
 	NO_ARGS: 'Please enter a song name/url',
 	TOO_HIGH: 'Please input a number between 0 and 100.',
 	NO_SONG: 'I can\'t find that song.',
+	INCORRECT_NUMBER: 'Speed can only be 1 to 10.',
 	// pause/resume
 	ALREADY_PAUSED: (prefix) => `I am already paused, \`${prefix}resume\` to carry on listening.`,
 	ALREADY_RESUMED: (prefix) => `I am already playing, \`${prefix}pause\` to carry on listening.`,
