214b802e511875acecc94b715de22411e04f50c3
Spiderjockey2 <benjamin.forey11gmail.com>
2021-03-02 22:45:02 +0000

    added emoji-list command

---
+++ src/commands/Guild/emoji-list.js
@@ -0,0 +1,22 @@
+// Dependencies
+const	Command = require('../../structures/Command.js');
+
+module.exports = class EmojiList extends Command {
+	constructor(bot) {
+		super(bot, {
+			name: 'emojilist',
+			guildOnly: true,
+			dirname: __dirname,
+			aliases: ['emoji-list'],
+			botPermissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
+			description: 'Displays the server\'s emojis',
+			usage: 'emojilist',
+			cooldown: 2000,
+		});
+	}
+
+	// Run command
+	async run(bot, message) {
+		message.channel.send(`**Server's emoji's:**\n${message.guild.emojis.cache.map(e => e.toString()).join(' ')}`);
+	}
+};
