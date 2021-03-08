eaea2467d18545d1c2673b7dcd7154e809aee8b0
Spiderjockey2 <benjamin.forey11gmail.com>
2021-03-08 16:53:58 +0000

    added help command

--- src/commands/Misc/help.js
+++ src/commands/Misc/help.js
@@ -2,19 +2,6 @@
 const { MessageEmbed } = require('discord.js'),
 	Command = require('../../structures/Command.js');
 
-// Check what plugins are running on the server
-function checkPlugins(settings) {
-	const plugins = [];
-	if (settings.MusicPlugin) plugins.push('Music');
-	if (settings.LevelPlugin) plugins.push('Level');
-	if (settings.ModerationPlugin) plugins.push('Moderation');
-	if (settings.SearchPlugin) plugins.push('Searcher');
-	if (settings.NSFWPlugin) plugins.push('Nsfw');
-	if (settings.MusicTriviaPlugin) plugins.push('Trivia');
-	return plugins;
-}
-
-
 module.exports = class Help extends Command {
 	constructor(bot) {
 		super(bot, {
@@ -29,23 +16,27 @@ module.exports = class Help extends Command {
 
 	// Run command
 	async run(bot, message, args, settings) {
-		const plugins = checkPlugins(settings);
 		if (!args[0]) {
 			// Show default help page
 			const embed = new MessageEmbed()
-				.setColor(3447003)
-				.setThumbnail(bot.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }))
-				.setTitle(`${bot.user.username}'s Plugin list.`)
-				.addField('Fun', `\`${settings.prefix}help Fun\``, true)
-				.addField('Guild', `\`${settings.prefix}help Guild\``, true)
-				.addField('Image', `\`${settings.prefix}help Image\``, true)
-				.addField('Misc', `\`${settings.prefix}help Misc\``, true)
-				.addField('Giveaway', `\`${settings.prefix}help Giveaway\``, true)
-				.addField('Plugins', `\`${settings.prefix}help Plugins\``, true);
-			for (let i = 0; i < plugins.length; i++) {
-				embed.addField(plugins[i], `\`${settings.prefix}help ${plugins[i]}\``, true);
-			}
-			embed.addField('Plugins', `\`${settings.prefix}help plugin-list\``, true);
+				.setAuthor('Available commands', bot.user.displayAvatarURL({ format: 'png' }))
+				.setDescription([
+					`**Prefix:** \`${settings.prefix}\` (You can also use <@!${bot.user.id}> as a prefix)`,
+					`**Type \`${settings.prefix}help [command name]\` for command specific information.**`,
+				].join('\n'));
+			const categories = bot.commands.map(c => c.help.category).filter((c) => settings.plugins.includes(c)).filter((v, i, a) => a.indexOf(v) === i);
+			categories
+				.sort((a, b) => a.category - b.category)
+				.forEach(category => {
+					const commands = bot.commands
+						.filter(c => c.help.category === category)
+						.sort((a, b) => a.help.name - b.help.name)
+						.map(c => `\`${c.help.name}\``).join('**, **');
+
+					const length = bot.commands
+						.filter(c => c.help.category === category).size;
+					embed.addField(`${category} [**${length}**]`, commands, false);
+				});
 			message.channel.send(embed);
 		} else if (args.length == 1) {
 			// Check if arg is command
@@ -53,7 +44,7 @@ module.exports = class Help extends Command {
 				// arg was a command
 				const cmd = bot.commands.get(args[0]) || bot.commands.get(bot.aliases.get(args[0]));
 				// Check if the command is allowed on the server
-				if (plugins.includes(cmd.help.category) || ['Fun', 'Guild', 'Image', 'Misc', 'Plugins'].includes(cmd.help.category) || message.author.id == bot.config.ownerID) {
+				if (settings.plugins.includes(cmd.help.category) || message.author.id == bot.config.ownerID) {
 					const embed = new MessageEmbed()
 						.setThumbnail(message.guild.iconURL())
 						.setAuthor(`${bot.user.username} HELP`, message.guild.iconURL)
@@ -69,30 +60,7 @@ module.exports = class Help extends Command {
 					message.error(settings.Language, 'MISC/NO_COMMAND');
 				}
 			} else {
-				// arg is likely to be plugin
-				const embed = new MessageEmbed()
-					.setTitle(`${args[0].charAt(0).toUpperCase() + args[0].slice(1)} Plugin`);
-				bot.commands.forEach(cmd => {
-					if (cmd.help.category == args[0].charAt(0).toUpperCase() + args[0].slice(1)) {
-						embed.addField(`\`${cmd.help.usage.replace('${PREFIX}', settings.prefix)}\``, cmd.help.description);
-					}
-				});
-				// Check if embed is empty
-				if (embed.fields.length != 0) {
-					message.channel.send(embed);
-				} else if (args[0].charAt(0).toUpperCase() + args[0].slice(1) == 'Plugin-list') {
-					const embed2 = new MessageEmbed()
-						.setTitle('Plugin list for this server')
-						.addField('Level plugin', `enabled: \`${settings.LevelPlugin}\``)
-						.addField('Music', `enabled: \`${settings.MusicPlugin}\``)
-						.addField('Moderation', `enabled: \`${settings.ModerationPlugin}\``)
-						.addField('NSFW', `enabled: \`${settings.NSFWPlugin}\``)
-						.addField('Search', `enabled: \`${settings.SearchPlugin}\``)
-						.addField('Trivia', `enabled: \`${settings.MusicTriviaPlugin}\``);
-					message.channel.send(embed2);
-				} else {
-					message.error(settings.Language, 'MISC/MISSING_COMMAND');
-				}
+				message.error(settings.Language, 'MISC/NO_COMMAND');
 			}
 		} else {
 			message.error(settings.Language, 'INCORRECT_FORMAT', settings.prefix.concat(this.help.usage));
