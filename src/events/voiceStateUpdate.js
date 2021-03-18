7a6e550a957c20e19e6db60468e4ed0b81d335c3
Spiderjockey2 <benjamin.forey11gmail.com>
2021-03-18 21:10:30 +0000

    Added check to make sure bot leaves after 3mins when lonely

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -48,4 +48,49 @@ module.exports = async (bot, oldState, newState) => {
 			if (modChannel) modChannel.send(embed);
 		}
 	}
+
+
+	// Only keep the bot in the voice channel by its self for 3 minutes
+	const player = bot.manager.players.get(newState.guild.id);
+
+	if (!player) return;
+	if (!newState.guild.members.cache.get(bot.user.id).voice.channelID) player.destroy();
+	if (oldState.id === bot.user.id) return;
+	if (!oldState.guild.members.cache.get(bot.user.id).voice.channelID) return;
+
+	// Make sure the bot is in the voice channel that 'activated' the event
+	if (oldState.guild.members.cache.get(bot.user.id).voice.channel.id === oldState.channelID) {
+		if (oldState.guild.voice.channel && oldState.guild.voice.channel.members.size === 1) {
+			const vcName = oldState.guild.me.voice.channel.name;
+			const embed = new MessageEmbed()
+				.setDescription(`Leaving **${vcName}** in ${180000 / 1000} seconds because I was left alone.`);
+			const c = bot.channels.cache.get(player.textChannel);
+			let msg;
+			if (c) {
+				try {
+					msg = await c.send(embed);
+				} catch (err) {
+					bot.logger.error(err.message);
+				}
+			}
+			const delay = ms => new Promise(res => setTimeout(res, ms));
+			await delay(180000);
+
+			const vcMembers = oldState.guild.voice.channel.members.size;
+			if (!vcMembers || vcMembers === 1) {
+				const newPlayer = bot.manager.players.get(newState.guild.id);
+				(newPlayer) ? player.destroy() : oldState.guild.voice.channel.leave();
+
+				const embed2 = new MessageEmbed()
+					.setDescription(`I left **${vcName}** because I was left alone.`);
+				try {
+					return msg.edit(embed2, '');
+				} catch (err) {
+					bot.logger.error(err.message);
+				}
+			} else {
+				return msg.delete();
+			}
+		}
+	}
 };
