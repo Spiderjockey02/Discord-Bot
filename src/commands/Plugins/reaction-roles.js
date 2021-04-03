49a66ccc49afd3936569a9ef57561da97a812237
Spiderjockey2 <benjamin.forey11gmail.com>
2021-04-03 19:43:13 +0100

    updated reaction role command

--- src/commands/Plugins/reaction-roles.js
+++ src/commands/Plugins/reaction-roles.js
@@ -1,148 +1,133 @@
-const Command = require("../../structures/Command.js");
-const ReactionsModel = require("../../modules/database/models/reactionrole");
-const { MessageEmbed } = require("discord.js");
+// Dependecies
+const Command = require('../../structures/Command.js'),
+	{ ReactionRoleSchema } = require('../../database/models'),
+	{ MessageEmbed } = require('discord.js');
+
 module.exports = class ReactionRoles extends Command {
-  constructor(bot) {
-    super(bot, {
-      name: "reactionroles",
-      dirname: __dirname,
-      aliases: ["reaction-roles"],
-      userPermissions: ["MANAGE_GUILD"],
-      botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
-      description: "Make reaction roles",
-      usage: "reactionroles <channel>",
-      cooldown: 5000,
-      examples: ["reactionroles 37844848481818441"],
-    });
-  }
+	constructor(bot) {
+		super(bot, {
+			name: 'reactionroles',
+			dirname: __dirname,
+			aliases: ['reaction-roles', 'rr'],
+			userPermissions: ['MANAGE_GUILD'],
+			botPermissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
+			description: 'Make reaction roles',
+			usage: 'reactionroles <channelID>',
+			cooldown: 5000,
+			examples: ['reactionroles 37844848481818441'],
+		});
+	}
 
-  // Run command
-  async run(bot, message, args, settings) {
-    let emojis;
-    let roles;
-    const [channelId] = args;
-    const { guild } = message;
-    const filter = (m) => message.author.id === m.author.id;
+	// Run command
+	async run(bot, message, args, settings) {
+		// Make sure user can edit server plugins
+		if (!message.member.hasPermission('MANAGE_GUILD')) return message.error(settings.Language, 'USER_PERMISSION', 'MANAGE_GUILD').then(m => m.delete({ timeout: 10000 }));
 
-    if (!channelId) {
-      return message
-        .error(
-          settings.Language,
-          "INCORRECT_FORMAT",
-          settings.prefix.concat(this.help.usage)
-        )
-        .then((m) => m.delete({ timeout: 5000 }));
-    }
-    message.channel.send(
-      message.translate(settings.language, "PLUGINS/SEND_ROLES")
-    );
+		// Make sure data was entered
+		if (!args[0]) return message.error(settings.Language, 'INCORRECT_FORMAT', settings.prefix.concat(this.help.usage)).then(m => m.delete({ timeout: 5000 }));
 
-    const roleMsgs = await message.channel.awaitMessages(filter, {
-      time: 600000,
-      max: 1,
-      errors: ["time"],
-    });
-    const roleMsg = roleMsgs.first();
-    roles = parseRoles(roleMsg, guild);
+		// Make sure channel is a text channel
+		const channel = message.guild.channels.cache.get(args[0]);
+		if (!channel || channel.type !== 'text') {
+			return message.error(settings.Language, 'MISSING_CHANNEL', settings.prefix.concat(this.help.usage));
+		} else if (!channel.permissionsFor(bot.user).has(['SEND_MESSAGES', 'EMBED_LINKS', 'ADD_REACTIONS'])) {
+			console.log('gfjhkgfdhjk');
+			return message.channel.send('Missing permissions');
+		}
 
-    message.channel.send(
-      message.translate(settings.language, "PLUGINS/SEND_EMOJIS")
-    );
+		// Get all roles mentioned
+		message.channel.send(message.translate(settings.Language, 'PLUGINS/SEND_ROLES'));
 
-    const emojiMsgs = await message.channel.awaitMessages(filter, {
-      time: 600000,
-      max: 1,
-      errors: ["time"],
-    });
-    const emojiMsg = emojiMsgs.first();
-    emojis = parseEmojis(emojiMsg);
+		const filter = (m) => message.author.id === m.author.id;
+		const roleMsgs = await message.channel.awaitMessages(filter, {
+			time: 600000,
+			max: 1,
+			errors: ['time'],
+		});
 
-    const channel = guild.channels.cache.get(channelId);
-    if (!channel) {
-      return message.channel.send(
-        message.error(
-          settings.Language,
-          "MISSING_ROLES",
-          settings.prefix.concat(this.help.usage)
-        )
-      );
-    }
+		const roleMsg = roleMsgs.first(),
+			roles = this.parseRoles(roleMsg, message.guild);
 
-    const embed = new MessageEmbed()
-      .setTitle(
-        message.translate(settings.language, "PLUGINS/EGGLORD_REACTIONS")
-      )
-      .setDescription(
-        message.translate(
-          settings.language,
-          "PLUGINS/REACT_BELOW",
-          createDescription(roles, emojis)
-        )
-      );
+		// Get all emojis mentioned
+		message.channel.send(message.translate(settings.Language, 'PLUGINS/SEND_EMOJIS'));
 
-    const msg = await channel.send(embed);
+		const emojiMsgs = await message.channel.awaitMessages(filter, {
+			time: 600000,
+			max: 1,
+			errors: ['time'],
+		});
 
-    emojis.forEach((em) => {
-      msg.react(em);
-    });
+		const emojiMsg = emojiMsgs.first(),
+			emojis = this.parseEmojis(emojiMsg);
 
-    const reactions = [];
+		// Now display message to chosen channel
+		const embed = new MessageEmbed()
+			.setTitle(message.translate(settings.Language, 'PLUGINS/EGGLORD_REACTIONS'))
+			.setDescription(message.translate(settings.Language, 'PLUGINS/REACT_BELOW', createDescription()));
 
-    for (let i = 0; i < roles.length; i++) {
-      reactions.push({ role_id: roles[i].id, emoji: emojis[i].toString() });
-    }
+		channel.send(embed).then(async (msg) => {
+			emojis.forEach(async (em) => {
+				await msg.react(em);
 
-    const newRR = new ReactionsModel({
-      guild_id: guild.id,
-      message_id: msg.id,
-      reactions: reactions,
-      channel_id: channelId,
-    });
+			});
 
-    newRR.save();
+			const reactions = [];
+			for (let i = 0; i < roles.length; i++) {
+				reactions.push({ role_id: roles[i].id, emoji: emojis[i].toString() });
+			}
 
-    return message.channel.send("Succes!");
-  }
+			const newRR = new ReactionRoleSchema({
+				guildID: message.guild.id,
+				channelID: channel.id,
+				messageID: msg.id,
+				reactions: reactions,
+			});
+
+			newRR.save();
+
+			// Tell user that reaction role creation was successfully
+			message.channel.send('Success!');
+		});
+
+		// create the description
+		function createDescription() {
+			const strings = [];
+			for (let i = 0; i < roles.length; i++) {
+				strings.push(`${message.guild.emojis.cache.get(emojis[i]).toString()}: ${roles[i]}`);
+			}
+
+			return strings.join('\n');
+		}
+	}
+
+
+	parseRoles(msg, guild) {
+		const content = msg.content.trim().split(/ +/g);
+
+		// Remove any duplicates
+		const filtered = [...new Set(content)];
+
+		let roles = [];
+
+		filtered.forEach(async (roleId) => {
+			const role = guild.roles.cache.get(roleId) || (await guild.roles.fetch(roleId));
+			roles = [...roles, role];
+			return role;
+		});
+
+		return roles;
+	}
+
+	parseEmojis(msg) {
+		let content = msg.content.trim().split(/ +/g);
+
+		content = content.filter((s) => {
+			// Remove custom emojis
+			if (s.split(':').length === 1 ? false : true) {
+				return false;
+			}
+			return true;
+		});
+		return [...new Set(content)];
+	}
 };
-function createDescription(roles, emojis) {
-  const strings = [];
-
-  for (let i = 0; i < roles.length; i++) {
-    strings.push(`${emojis[i]}: ${roles[i]}`);
-  }
-
-  return strings.join("\n");
-}
-
-function parseRoles(msg, guild) {
-  const content = msg.content.trim().split(/ +/g);
-
-  // Remove any duplicates
-  const filtered = [...new Set(content)];
-
-  let roles = [];
-
-  filtered.forEach(async (roleId) => {
-    const role =
-      guild.roles.cache.get(roleId) || (await guild.roles.fetch(roleId));
-
-    roles = [...roles, role];
-    return role;
-  });
-
-  return roles;
-}
-
-function parseEmojis(msg) {
-  let content = msg.content.trim().split(/ +/g);
-
-  content = content.filter((s) => {
-    // Remove custom emojis
-    if (s.split(":").length === 1 ? false : true) {
-      return false;
-    }
-    return true;
-  });
-
-  return [...new Set(content)];
-}
