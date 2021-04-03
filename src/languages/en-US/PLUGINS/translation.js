3eeb22363669d65f12de94b7ce43d3aa8434fdec
Spiderjockey2 <benjamin.forey11gmail.com>
2021-04-03 18:00:43 +0100

    updated languages

--- src/languages/en-US/PLUGINS/translation.js
+++ src/languages/en-US/PLUGINS/translation.js
@@ -1,31 +1,27 @@
 const languageData = {
-  // SUCCESS
-  LANGUAGE_SET: (language) => `Language has been set to \`${language}\`.`,
-  LEVEL_SET: (option) => `Level plugin has been set to \`${option}\`.`,
-  MODERATION_SET: (option) =>
-    `Moderation plugin has been set to \`${option}\`.`,
-  MUSIC_SET: (option) => `Music plugin has been set to \`${option}\`.`,
-  NSFW_SET: (option) => `NSFW plugin has been set to \`${option}\`.`,
-  SEARCH_SET: (option) => `Search plugin has been set to \`${option}\`.`,
-  LOGS_SET: (option) => `Logging plugin has been set to \`${option}\`.`,
-  // error message
-  MISSING_LANGUAGE: "No language selected.",
-  NO_LANGUAGE:
-    "That is not a language I support, yet. Why not help me learn that language by joining here: https://discord.gg/8g6zUQu",
-  // other success messages
-  LOG_CHANNEL: (channelID) => `Logging channel updated to: <#${channelID}>`,
-  SEND_ROLES:
-    "Please send your roles by id below, separate by space. E.G.: 389730847098379087 9876096987980987 7867869876689766",
-  SEND_EMOJIS:
-    "Please send your emojis below. The order will match with the order of the roles. Separate with a space",
-  EGGLORD_REACTIONS: "Egglord Reaction Roles",
-  REACT_BELOW: (description) => `React below to get a role!\n${description}`,
+	// SUCCESS
+	LANGUAGE_SET: (language) => `Language has been set to \`${language}\`.`,
+	LEVEL_SET: (option) => `Level plugin has been set to \`${option}\`.`,
+	MODERATION_SET: (option) => `Moderation plugin has been set to \`${option}\`.`,
+	MUSIC_SET: (option) => `Music plugin has been set to \`${option}\`.`,
+	NSFW_SET: (option) => `NSFW plugin has been set to \`${option}\`.`,
+	SEARCH_SET: (option) => `Search plugin has been set to \`${option}\`.`,
+	LOGS_SET: (option) => `Logging plugin has been set to \`${option}\`.`,
+	LOG_CHANNEL: (channelID) => `Logging channel updated to: <#${channelID}>`,
+	// error message
+	MISSING_LANGUAGE: 'No language selected.',
+	NO_LANGUAGE: 'That is not a language I support, yet. Why not help me learn that language by joining here: https://discord.gg/8g6zUQu',
+	// Reaction role messages
+	SEND_ROLES: 'Please send your roles by id below, separate by space. E.G.: 389730847098379087 9876096987980987 7867869876689766',
+	SEND_EMOJIS: 'Please send your emojis below. The order will match with the order of the roles. Separate with a space',
+	EGGLORD_REACTIONS: 'Reaction Roles:',
+	REACT_BELOW: (description) => `React below to get a role!\n${description}`,
 };
 
 const translate = (key, args) => {
-  const translation = languageData[key];
-  if (typeof translation === "function") return translation(args);
-  else return translation;
+	const translation = languageData[key];
+	if (typeof translation === 'function') return translation(args);
+	else return translation;
 };
 
 module.exports = translate;
--- src/languages/en-US/misc.js
+++ src/languages/en-US/misc.js
@@ -1,30 +1,25 @@
 // For better permissions
-const permissions = require("../../utils/permissions.json");
+const permissions = require('../../utils/permissions.json');
 
 // languageData
 const languageData = {
-  ERROR_MESSAGE: (error) =>
-    `The following error has occurred: \`${error}\`.\nIf this error keeps occurring, please contact support.`,
-  INCORRECT_FORMAT: (commandExample) =>
-    `Please use the format: \`${commandExample}\`.`,
-  MISSING_PERMISSION: (permission) =>
-    `I am missing the permission: \`${permissions[permission]}\`.`,
-  USER_PERMISSION: (permission) =>
-    `You are missing the permission: \`${permissions[permission]}\`.`,
-  MISSING_ROLE: "I was unable to find this role.",
-  NO_REASON: "No reason given.",
-  // external files/plugins
-  INCORRECT_DELIMITERS:
-    "Please use one of the following time delimiters: `d`,  `h`,  `m`, `s`.",
-  NOT_NUMBER: "Must be a number.",
-  MAX_TIME: "Can't be longer than 10 days.",
-  MISSING_CHANNEL: "I was unable to find this channel.",
+	ERROR_MESSAGE: (error) =>`The following error has occurred: \`${error}\`.\nIf this error keeps occurring, please contact support.`,
+	INCORRECT_FORMAT: (commandExample) => `Please use the format: \`${commandExample}\`.`,
+	MISSING_PERMISSION: (permission) => `I am missing the permission: \`${permissions[permission]}\`.`,
+	USER_PERMISSION: (permission) => `You are missing the permission: \`${permissions[permission]}\`.`,
+	MISSING_ROLE: 'I was unable to find this role.',
+	NO_REASON: 'No reason given.',
+	// external files/plugins
+	INCORRECT_DELIMITERS: 'Please use one of the following time delimiters: `d`,  `h`,  `m`, `s`.',
+	NOT_NUMBER: 'Must be a number.',
+	MAX_TIME: 'Can\'t be longer than 10 days.',
+	MISSING_CHANNEL: 'I was unable to find this channel.',
 };
 
 const translate = (key, args) => {
-  const translation = languageData[key];
-  if (typeof translation === "function") return translation(args);
-  else return translation;
+	const translation = languageData[key];
+	if (typeof translation === 'function') return translation(args);
+	else return translation;
 };
 
 module.exports = translate;
