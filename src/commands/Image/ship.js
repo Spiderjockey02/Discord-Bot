ed136461567da4d7998d523595fb36122def6b34
Spiderjockey2 <benjamin.forey11gmail.com>
2021-04-04 12:21:38 +0100

    fixed ship command

--- src/commands/Image/ship.js
+++ src/commands/Image/ship.js
@@ -20,13 +20,14 @@ module.exports = class Ship extends Command {
 	async run(bot, message, args, settings) {
 		// Get image, defaults to author's avatar
 		const users = message.guild.GetImage(message, args, settings.Language);
+		if (!users[1]) return message.error(settings.Language, 'INCORRECT_FORMAT', settings.prefix.concat(this.help.usage)).then(m => m.delete({ timeout: 5000 }));
 
 		// send 'waitng' message
 		const msg = await message.sendT(settings.Language, 'IMAGE/GENERATING_IMAGE');
 
 		// Try and convert image
 		try {
-			const res = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=ship&user1=${users[0].user.displayAvatarURL({ format: 'png', size: 512 })}&user2=${users[1].user.displayAvatarURL({ format: 'png', size: 512 })}`));
+			const res = await fetch(encodeURI(`https://nekobot.xyz/api/imagegen?type=ship&user1=${users[0]}&user2=${users[1]}`));
 			const json = await res.json();
 
 			// send image
