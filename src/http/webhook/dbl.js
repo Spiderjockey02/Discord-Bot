1be22289e70b75b9f34b48daeb9904cdbda60b6f
Spiderjockey2 <benjamin.forey11gmail.com>
2021-04-07 22:58:57 +0100

    updated DBL webhhook

--- src/http/webhook/dbl.js
+++ src/http/webhook/dbl.js
@@ -1,23 +1,22 @@
+// Dependencies
 const DBL = require('dblapi.js');
+
 module.exports = bot => {
 	const dbl = new DBL(bot.config.DiscordBotLists.DBL_KEY, { webhookPort: 8000, webhookAuth: 'anyPassword' });
 
 	dbl.webhook.on('ready', hook => {
-		console.log(`Webhook up and running at http://${hook.hostname}:${hook.port}${hook.path}`);
+		bot.logger.log(`DBL webhook running on: http://${hook.hostname}:${hook.port}${hook.path}`);
 	});
 
 	// This will just log errors if there are any
-	dbl.on('error', e => {
-		console.log(`Oops! ${e}`);
+	dbl.on('error', err => {
+		bot.logger.error(`An error has occured: ${err.message}`);
 	});
 
 	// When the webhook receives a vote
 	dbl.webhook.on('vote', async vote => {
 		// This will log the whole vote object to the console
 		console.log(vote);
-		// Get the Discord ID of the user who voted
-		const userID = vote.user;
-		// If the channel to send messages in exists, we send a message in it with the ID of the user who votes
-		// if(channelForWebhooks) await channelForWebhooks.send(`User with ID \`${userID}\` just voted!`);
+		// Handle the vote how you want
 	});
 };
