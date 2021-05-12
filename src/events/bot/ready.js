0a840c896ebe641a982e89ed07acbbae92ad4ff5
Spiderjockey2 <benjamin.forey11gmail.com>
2021-05-12 20:21:04 +0100

    removed DBL webhook

--- src/events/bot/ready.js
+++ src/events/bot/ready.js
@@ -28,7 +28,6 @@ module.exports = class Ready extends Event {
 		// set up webserver
 		try {
 			require('../../http/api')(bot);
-			// require('../../http/webhook/dbl')(bot);
 		} catch (err) {
 			console.log(err);
 		}
