59abdf168a42e4df993d7235903e37ebf778f822
Spiderjockey2 <benjamin.forey11gmail.com>
2021-05-15 00:27:39 +0100

    added support for clear command up to 1000 messages

--- src/commands/Moderation/clear.js
+++ src/commands/Moderation/clear.js
@@ -1,5 +1,7 @@
 // Dependencies
-const Command = require('../../structures/Command.js');
+const { MessageEmbed } = require('discord.js'),
+	delay = ms => new Promise(res => setTimeout(res, ms)),
+	Command = require('../../structures/Command.js');
 
 module.exports = class Clear extends Command {
 	constructor(bot) {
@@ -44,20 +46,72 @@ module.exports = class Clear extends Command {
 		if (!amount) return message.channel.error(settings.Language, 'INCORRECT_FORMAT', settings.prefix.concat(this.help.usage)).then(m => m.delete({ timeout: 5000 }));
 
 		// Make sure x is a number
-		if (isNaN(amount) || (amount > 100) || (amount < 1)) return message.channel.error(settings.Language, 'INCORRECT_FORMAT', settings.prefix.concat(this.help.usage)).then(m => m.delete({ timeout: 5000 }));
+		if (isNaN(amount) || (amount > 1000) || (amount < 1)) return message.channel.error(settings.Language, 'INCORRECT_FORMAT', settings.prefix.concat(this.help.usage)).then(m => m.delete({ timeout: 5000 }));
 
+		// make sure guild is premium if amount > 200
+		if (amount > 200 && !message.guild.premium) return message.channel.send('The server must be premium in order to clear more than `200` messages.');
 
-		// Delete messages
-		await message.channel.messages.fetch({ limit: amount }).then(async messages => {
-			// Delete user messages
-			if (message.args[1]) {
-				const member = message.getMember();
-				messages = messages.filter((m) => m.author.id == member[0].user.id);
-			}
+		// Confirmation for message deletion over 100
+		if (amount >= 100) {
+			const embed = new MessageEmbed()
+				.setTitle('Clear Message Confirmation')
+				.setDescription(`You are about to clear ${amount} messages from this channel, please react to confirm this action.`)
+				.setTimestamp();
 
-			// delete the message
-			await message.channel.bulkDelete(messages, true).catch(err => bot.logger.error(`Command: '${this.help.name}' has error: ${err.message}.`));
-			message.channel.success(settings.Language, 'MODERATION/MESSAGES_DELETED', messages.size).then(m => m.delete({ timeout: 3000 }));
-		});
+			message.channel.send(embed).then(async confirmationMessage => {
+				// React to message
+				await confirmationMessage.react('✅');
+
+				// filter
+				const filter = (reaction, user) => {
+					return reaction.emoji.name === '✅' && user.id === message.author.id;
+				};
+
+				// Collect the reactions
+				const collector = confirmationMessage.createReactionCollector(filter, { time: 15000 });
+				collector.on('collect', async () => {
+					let x = amount, y = 0;
+					while (x !== 0) {
+						try {
+							let messages = await message.channel.messages.fetch({ limit: x > 100 ? 100 : x });
+							// Delete user messages
+							if (message.args[1]) {
+								const member = message.getMember();
+								messages = messages.filter((m) => m.author.id == member[0].user.id);
+							}
+
+							// delete the message
+							const delMessages = await message.channel.bulkDelete(messages, true).catch(err => bot.logger.error(`Command: '${this.help.name}' has error: ${err.message}.`));
+							y += delMessages.size;
+							x = x - messages.size;
+							await delay(5000);
+						} catch (e) {
+							break;
+						}
+					}
+					message.channel.success(settings.Language, 'MODERATION/MESSAGES_DELETED', y).then(m => m.delete({ timeout: 3000 }));
+				});
+
+				// The user did not respond in time
+				collector.on('end', async () => {
+					await confirmationMessage.reactions.removeAll();
+					embed.setDescription('Confirmation timed-out.');
+					confirmationMessage.edit(embed);
+				});
+			});
+		} else {
+			// Delete messages (less than 100)
+			await message.channel.messages.fetch({ limit: amount }).then(async messages => {
+				// Delete user messages
+				if (message.args[1]) {
+					const member = message.getMember();
+					messages = messages.filter((m) => m.author.id == member[0].user.id);
+				}
+
+				// delete the message
+				await message.channel.bulkDelete(messages, true).catch(err => bot.logger.error(`Command: '${this.help.name}' has error: ${err.message}.`));
+				message.channel.success(settings.Language, 'MODERATION/MESSAGES_DELETED', messages.size).then(m => m.delete({ timeout: 3000 }));
+			});
+		}
 	}
 };
