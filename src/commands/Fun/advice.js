9b88ec355a3d21d5f59a0f96ab16842ec4c8c87a
Wolfie <Wolfkid200444@gmail.com>
2021-05-20 21:53:04 -0400

    test

--- src/commands/Fun/advice.js
+++ src/commands/Fun/advice.js
@@ -28,7 +28,7 @@ module.exports = class Advice extends Command {
 			if (message.deletable) message.delete();
 			bot.logger.error(`Command: '${this.help.name}' has error: ${err.message}.`);
 			msg.delete();
-			message.channel.error(settings.Language, 'ERROR_MESSAGE', err.message).then(m => m.delete({ timeout: 5000 }));
+			message.channel.error(settings.Language, 'ERROR_MESSAGE', err.message).then(m => setTimeout(() => { m.delete(); }, 5000));
 		}
 	}
 };
