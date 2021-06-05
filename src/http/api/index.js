eb0c21e3df093ee9bb9e9bdcb59b1d16a8bbf78e
Spiderjockey2 <benjamin.forey11gmail.com>
2021-06-05 13:17:20 +0100

    updated API

--- src/http/api/index.js
+++ src/http/api/index.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'),
+ app = express(),
+ port = 3000,
+ cors = require('cors');
 
 module.exports = bot => {
 	app
 		// Home page
+		.use(cors())
 		.get('/', (req, res) => {
 			res.type('text/plain');
 			res.send(`API server for ${bot.user.username}
--- src/http/api/statistics.js
+++ src/http/api/statistics.js
@@ -1,15 +1,21 @@
-// Dependecies
+// Dependencies
 const express = require('express'),
 	router = express.Router();
 
-module.exports = function(bot) {
+let totalUsers
+module.exports = (bot) => {
 	// statistics page
 	router.get('/', async function(req, res) {
 		if (bot.config.debug) bot.logger.debug(`IP: ${req.connection.remoteAddress.slice(7)} accessed \`/statistics\`.`);
 
+		bot.guilds.cache.forEach(guild => {
+			totalUsers = totalUsers + ( guild.memberCount ?  guild.memberCount : 0)
+    })
+
 		res.status(200).json({
 			guildCount: bot.guilds.cache.size,
-			userCount: bot.users.cache.size,
+			cachedUsers: bot.users.cache.size,
+			totalMembers: totalUsers,
 			uptime: process.uptime() * 1000,
 			commandCount: bot.commands.size,
 			memoryUsed: (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2),
@@ -17,6 +23,7 @@ module.exports = function(bot) {
 			voiceChannels: bot.channels.cache.filter(channel => channel.type === 'voice').size,
 			MessagesSeen: bot.messagesSent,
 			CommandsRan: bot.commandsUsed,
+			ping: Math.round(bot.ws.ping),
 		});
 	});
 
