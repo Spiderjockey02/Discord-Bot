e9b7d447491ea1f473149f626ba996966d23ebcf
Spiderjockey2 <benjamin.forey11gmail.com>
2021-08-24 22:13:55 +0100

    fixed music error when member isn't in vc and uses music cmd

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,16 +2,19 @@ module.exports.checkMusic = (member, bot) => {
 	// Check if the member has role to interact with music plugin
 	if (member.guild.roles.cache.get(member.guild.settings.MusicDJRole)) {
 		if (!member.roles.cache.has(member.guild.settings.MusicDJRole)) {
-			return bot.translate('misc:MISSING_ROLE', {}, member.guild.settings.Language);
+			return member.guild.translate('misc:MISSING_ROLE');
 		}
 	}
 
 	// Check that a song is being played
 	const player = bot.manager?.players.get(member.guild.id);
-	if (!player) return bot.translate('misc:NO_QUEUE', {}, member.guild.settings.Language);
+	if (!player) return member.guild.translate('misc:NO_QUEUE');
+
+	// make sure user is in a voice channel
+	if (!member.voice.channel) return member.guild.translate('music/play:NOT_VC');
 
 	// Check that user is in the same voice channel
-	if (member.voice.channel.id !== player.voiceChannel) return bot.translate('misc:NOT_VOICE', {}, member.guild.settings.Language);
+	if (member.voice.channel.id !== player.voiceChannel) return member.guild.translate('misc:NOT_VOICE');
 
 	return true;
 };
