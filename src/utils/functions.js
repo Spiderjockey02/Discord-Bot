f23e5b98b7e76bc7bc65fae087a9f8cb5d7e8858
Spiderjockey02 <benjamin.forey11@gmail.com>
2022-04-11 00:37:48 +0100

    Makes sure user is in a VC before checking DJ role

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,17 +1,17 @@
 module.exports.checkMusic = (member, bot) => {
-	// Check if the member has role to interact with music plugin
-	if (member.guild.roles.cache.get(member.guild.settings.MusicDJRole)) {
-		if (!member.roles.cache.has(member.guild.settings.MusicDJRole)) {
-			return member.guild.translate('misc:MISSING_ROLE');
-		}
-	}
-
 	// Check that a song is being played
 	const player = bot.manager?.players.get(member.guild.id);
 	if (!player) return member.guild.translate('misc:NO_QUEUE');
 
 	// Check that user is in the same voice channel
 	if (member.voice?.channel?.id !== player.voiceChannel) return member.guild.translate('misc:NOT_VOICE');
+
+	// Check if the member has role to interact with music plugin
+	if (member.guild.roles.cache.get(member.guild.settings.MusicDJRole)) {
+		if (!member.roles.cache.has(member.guild.settings.MusicDJRole)) {
+			return member.guild.translate('misc:MISSING_ROLE');
+		}
+	}
 
 	return true;
 };
