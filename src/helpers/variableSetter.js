9082eabf1372953535cd25fb6b652588a1787339
Spiderjockey02 <benjamin.forey11@gmail.com>
2022-04-28 13:10:34 +0100

    Updated custom variable code

--- src/helpers/variableSetter.js
+++ src/helpers/variableSetter.js
@@ -6,35 +6,42 @@ const constants = {
 };
 
 module.exports = (string, member, channel, guild) => {
-	const regex = /{(server|channel|user|member).[a-zA-Z]*}/g,
+	const regex = /{(server|channel|user|member)(.[a-zA-Z]*)?}/g,
 		chars = string.match(regex);
 	let x = 0;
 
-	if (chars.length >= 1) {
+	if (chars?.length >= 1) {
 		while (x < chars.length) {
-			console.log(chars[x]);
-			const type = chars[x].split('.')[0].substring(1);
-			switch (type) {
+			// Uses positive look ahead to extract the type from the variable (e.g {user} & {user.mention}  == 'user')
+			const type = chars[x].match(/(?<=\{)(server|channel|user|member)(?=\}|.)/g);
+			switch (type[0]) {
 				case 'server': {
-					const value = chars[x].split('.')[1].substring(0, chars[x].split('.')[1].length - 1);
+					const type2 = chars[x].replace(`{${type[0]}`, '');
+					const value = type2.substring(1, type2.length - 1);
 					// Replace variable with value
-					if (constants[type].includes(value)) 	string = string.replace(chars[x], guild[value]);
+					if (constants[type].includes(value)) string = string.replace(chars[x], guild[value]);
 					break;
 				}
 				case 'channel': {
-					const value = chars[x].split('.')[1].substring(0, chars[x].split('.')[1].length - 1);
+					const type2 = chars[x].replace(`{${type[0]}`, '');
+					const value = type2.substring(1, type2.length - 1);
 					// Replace variable with value
 					if (constants[type].includes(value)) string = string.replace(chars[x], channel[value]);
 					break;
 				}
 				case 'user': {
-					const value = chars[x].split('.')[1].substring(0, chars[x].split('.')[1].length - 1);
-					// Replace variable with value
-					if (constants[type].includes(value)) string = string.replace(chars[x], member.user[value]);
+					const type2 = chars[x].replace(`{${type[0]}`, '');
+					const value = type2.substring(1, type2.length - 1);
+					if (value.length == 1) {
+						string = string.replace(chars[x], `<@${member.id}>`);
+					} else if (constants[type].includes(value)) {
+						string = string.replace(chars[x], member.user[value]);
+					}
 					break;
 				}
 				case 'member': {
-					const value = chars[x].split('.')[1].substring(0, chars[x].split('.')[1].length - 1);
+					const type2 = chars[x].replace(`{${type[0]}`, '');
+					const value = type2.substring(1, type2.length - 1);
 					// Replace variable with value
 					if (constants[type].includes(value)) string = string.replace(chars[x], member[value]);
 					break;
