c4110dd82ef32e3e254f20cdbc8ea99a69bb2c2e
Halle Studios <info@louisplayz.xyz>
2022-08-26 10:54:03 +0200

    back to normal

--- src/events/guild/guildMemberAdd.js
+++ src/events/guild/guildMemberAdd.js
@@ -1,10 +1,8 @@
 // Dependencies
 const { Embed } = require('../../utils'),
+	varSetter = require('../../helpers/variableSetter'),
 	Event = require('../../structures/Event');
-const Canvas = require("canvas");
-const Discord = require("discord.js");
-const { registerFont, createCanvas } = require('canvas');
-registerFont('./compose-black.ttf', { family: 'Compose Black' });
+
 /**
  * Guild member add event
  * @event Egglord#GuildMemberAdd
@@ -18,7 +16,7 @@ class GuildMemberAdd extends Event {
 	}
 
 	/**
-	 * Function for recieving event.
+	 * Function for receiving event.
 	 * @param {bot} bot The instantiating client
 	 * @param {GuildMember} member The member that has joined a guild
 	 * @readonly
@@ -55,62 +53,7 @@ class GuildMemberAdd extends Event {
 		// Welcome plugin (give roles and message)
 		if (settings.welcomePlugin) {
 			const channel = member.guild.channels.cache.get(settings.welcomeMessageChannel);
-			if (channel) {
-			const canvas = Canvas.createCanvas(1440, 488);
-			const ctx = canvas.getContext('2d');
-			this.avatar = member.user.displayAvatarURL({ format: 'png' });
-            this.username = member.user.username + '#' + member.user.discriminator;
-            this.circleColor = "#FC5430";
-            this.mainText = "Welcome!";
-            this.mainTextColor = "#fff";
-            this.secondText = `We Are Now ${member.guild.memberCount} Members In The Server!`;
-            this.secondTextColor = "#FC5430";
-            this.pseudoColor = "#fff";
-            this.background = "./welcome.png";
-			const background = await Canvas.loadImage(this.background);
-            ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-			ctx.font = '80px "Compose Black"';
-			ctx.fillStyle = this.mainTextColor;
-			ctx.textAlign = "center";
-			ctx.shadowColor = "black";
-            ctx.shadowOffsetX = 10;
-            ctx.shadowOffsetY = 10;
-            ctx.shadowBlur = 15;
-			ctx.fillText(this.mainText.toUpperCase(), 771, 345);
-
-			ctx.font = '60px "Compose Black"';
-			ctx.fillStyle = this.pseudoColor;
-			ctx.textAlign = "center";
-			ctx.fillText(this.username.toUpperCase(), 771, 400);
-
-			ctx.font = '35px "Compose Black"';
-			ctx.fillStyle = this.secondTextColor;
-			ctx.textAlign = "center";
-			ctx.fillText(this.secondText.toUpperCase(), 771, 450);
-
-			ctx.beginPath();
-			ctx.fillStyle = this.circleColor;
-            ctx.shadowColor = "#fff";
-			ctx.arc(771, 140, 126, 0, Math.PI * 2);
-			ctx.fill();
-			ctx.closePath();
-
-			ctx.beginPath();
-			ctx.arc(771, 140, 118, 0, Math.PI * 2, true);
-			ctx.closePath();
-			ctx.clip();
-	        const avatar = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'jpg' }));
-			ctx.drawImage((avatar), 652, 22, 237, 237);// start, margin,width, height
-			const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "welcome.jpg");
-				try {
-					channel.send({
-						content: `${member.toString()} **Welcome to ${member.guild.name}!** `,
-						files: [attachment]
-					  });
-				} catch (err) {
-					bot.logger.error(`Event: '${this.conf.name}' has error: ${err.message}.`);
-				}
-} // #region channel.send(settings.welcomeMessageText.replace('{user}', member.user).replace('{server}', member.guild.name)).catch(e => bot.logger.error(e.message));
+			if (channel) channel.send(varSetter(settings.welcomeMessageText, member, channel, member.guild)).catch(e => bot.logger.error(e.message));
 			// Send private message to user
 			if (settings.welcomePrivateToggle) member.send(settings.welcomePrivateText.replace('{user}', member.user).replace('{server}', member.guild.name)).catch(e => bot.logger.error(e.message));
 
