3b25e564c177edcb52a3ff495ccb3bfef82dfe25
Spiderjockey02 <benjamin.forey11@gmail.com>
2022-09-11 17:35:14 +0100

    Updated embedColor in config as Random is invalid now

--- src/config.example.js
+++ src/config.example.js
@@ -59,7 +59,7 @@ const config = {
 	// URL to mongodb
 	MongoDBURl: 'mongodb://link',
 	// embed colour
-	embedColor: Colors.Random,
+	embedColor: Colors.Default,
 	// This will spam your console if you enable this but will help with bug fixing
 	debug: false,
 };
