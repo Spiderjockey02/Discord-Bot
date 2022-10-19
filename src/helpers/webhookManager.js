c6bc811577c52cf27e21fbb0d632e05589a72f77
Abner Silva <41455280+vSilva-gb@users.noreply.github.com>
2022-10-19 20:16:35 -0300

    Update webhookManager.js
    The createWebhook function now receives an object containing name and avatar instead of a string and the object

--- src/helpers/webhookManager.js
+++ src/helpers/webhookManager.js
@@ -1,40 +1,40 @@
 module.exports = async (bot) => {
-	// get list of channel ID's
-	const channelIDs = Array.from(bot.embedCollection.keys());
+    // get list of channel ID's
+    const channelIDs = Array.from(bot.embedCollection.keys());
 
-	// loop through each channel ID sending their embeds
-	for (const channel of channelIDs) {
-		try {
-			const webhooks = await bot.channels.fetch(channel).then(c => c.fetchWebhooks());
-			let webhook = webhooks.find(wh => wh.name == bot.user.username);
+    // loop through each channel ID sending their embeds
+    for (const channel of channelIDs) {
+        try {
+            const webhooks = await bot.channels.fetch(channel).then(c => c.fetchWebhooks());
+            let webhook = webhooks.find(wh => wh.name == bot.user.username);
 
-			// create webhook if it doesn't exist
-			if (!webhook) {
-				webhook = await bot.channels.fetch(channel).then(c => c.createWebhook(bot.user.username, {
-					avatar: bot.user.displayAvatarURL({ format: 'png', size: 1024 }),
-				}));
-			}
+            // create webhook if it doesn't exist
+            if (!webhook) {
+                webhook = await bot.channels.fetch(channel).then(c => c.createWebhook({ name: bot.user.username, 
+                    avatar: bot.user.displayAvatarURL({ format: 'png', size: 1024 }),
+                }));
+            }
 
-			// send the embeds
-			const repeats = Math.ceil(bot.embedCollection.get(channel).length / 10);
-			for (let j = 0; j < repeats; j++) {
-				// Get embeds and files to upload via webhook
-				const embeds = bot.embedCollection.get(channel)?.slice(j * 10, (j * 10) + 10).map(f => f[0]);
-				const files = bot.embedCollection.get(channel)?.slice(j * 10, (j * 10) + 10).map(f => f[1]).filter(e => e != undefined);
-				if (!embeds) return;
+            // send the embeds
+            const repeats = Math.ceil(bot.embedCollection.get(channel).length / 10);
+            for (let j = 0; j < repeats; j++) {
+                // Get embeds and files to upload via webhook
+                const embeds = bot.embedCollection.get(channel)?.slice(j * 10, (j * 10) + 10).map(f => f[0]);
+                const files = bot.embedCollection.get(channel)?.slice(j * 10, (j * 10) + 10).map(f => f[1]).filter(e => e != undefined);
+                if (!embeds) return;
 
-				// send webhook message
-				await webhook.send({
-					embeds: embeds,
-					files: files,
-				});
-			}
-			// delete from collection once sent
-			bot.embedCollection.delete(channel);
-		} catch (err) {
-			// It was likely they didn't have permission to create/send the webhook
-			bot.logger.error(err.message);
-			bot.embedCollection.delete(channel);
-		}
-	}
+                // send webhook message
+                await webhook.send({
+                    embeds: embeds,
+                    files: files,
+                });
+            }
+            // delete from collection once sent
+            bot.embedCollection.delete(channel);
+        } catch (err) {
+            // It was likely they didn't have permission to create/send the webhook
+            console.log(err);
+            bot.embedCollection.delete(channel);
+        }
+    }
 };
