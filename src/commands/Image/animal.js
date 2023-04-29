2ba5c2a93e774cd5fbab43ac0cca8c3c149cd003
Spiderjockey02 <benjamin.forey11@gmail.com>
2023-04-30 00:40:01 +0100

    Replaced cat and dog cmd for generic animal command (234 animals)

--- src/commands/Image/animal.js
+++ src/commands/Image/animal.js
@@ -1,26 +1,33 @@
 // Dependencies
 const { Embed } = require('../../utils'),
-	fetch = require('node-fetch'),
+	{ ApplicationCommandOptionType } = require('discord.js'),
 	Command = require('../../structures/Command.js');
 
 /**
  * Cat command
  * @extends {Command}
 */
-class Cat extends Command {
+class Animal extends Command {
 	/**
  	 * @param {Client} client The instantiating client
  	 * @param {CommandData} data The data for the command
 	*/
 	constructor(bot) {
 		super(bot, {
-			name: 'cat',
+			name: 'animal',
 			dirname: __dirname,
 			aliases: ['meow'],
 			description: 'Have a nice picture of a cat.',
-			usage: 'cat',
+			usage: 'animal',
 			cooldown: 2000,
 			slash: true,
+			options: [{
+				name: 'name',
+				description: 'The name of the animal',
+				type: ApplicationCommandOptionType.String,
+				required: true,
+				autocomplete: true,
+			}],
 		});
 	}
 
@@ -35,21 +42,13 @@ class Cat extends Command {
 		const msg = await message.channel.send(message.translate('misc:FETCHING', {
 			EMOJI: message.channel.checkPerm('USE_EXTERNAL_EMOJIS') ? bot.customEmojis['loading'] : '', ITEM: this.help.name }));
 
-		const res = await fetch('https://nekos.life/api/v2/img/meow')
-			.then(info => info.json())
-			.catch(err => {
-				// An error occured when looking for image
-				if (message.deletable) message.delete();
-				bot.logger.error(`Command: '${this.help.name}' has error: ${err.message}.`);
-				msg.delete();
-				return message.channel.error('misc:ERROR_MESSAGE', { ERROR: err.message });
-			});
+		const imageURL = await bot.fetch('misc/animal', { name: message.args[0] });
 
 		msg.delete();
 		// send image
 		const embed = new Embed(bot, message.guild)
 			.setColor(3426654)
-			.setImage(res.url);
+			.setImage(imageURL);
 		message.channel.send({ embeds: [embed] });
 	}
 
@@ -60,22 +59,37 @@ class Cat extends Command {
  	 * @param {guild} guild The guild the interaction ran in
  	 * @readonly
 	*/
-	async callback(bot, interaction, guild) {
-		const channel = guild.channels.cache.get(interaction.channelId);
+	async callback(bot, interaction, guild, args) {
+		const channel = guild.channels.cache.get(interaction.channelId),
+			name = args.get('name')?.value;
+
 		await interaction.reply({ content: guild.translate('misc:GENERATING_IMAGE', {
 			EMOJI: bot.customEmojis['loading'] }) });
+
 		try {
-			const res = await fetch('https://nekos.life/api/v2/img/meow').then(info => info.json());
+			const imageURL = await bot.fetch('misc/animal', { name });
+
 			// send image
 			const embed = new Embed(bot, guild)
 				.setColor(3426654)
-				.setImage(res.url);
+				.setImage(imageURL);
 			interaction.editReply({ content: ' ', embeds: [embed] });
 		} catch(err) {
 			bot.logger.error(`Command: '${this.help.name}' has error: ${err.message}.`);
 			return interaction.editReply({ embeds: [channel.error('misc:ERROR_MESSAGE', { ERROR: err.message }, true)], ephemeral: true });
 		}
 	}
+
+	async autocomplete(bot, interaction) {
+		// Get current input and make sure it's not 0
+		const searchQuery = interaction.options.getFocused(true).value;
+		if (searchQuery.length == 0) return interaction.respond([]);
+
+		// Fetch animal list and filter based on 'startsWith'
+		const animalList = await bot.fetch('misc/animal/raw');
+		const slctdAnimals = animalList.filter(i => i.toLowerCase().startsWith(searchQuery.toLowerCase())).slice(0, 10);
+		interaction.respond(slctdAnimals.map(i => ({ name: i, value: i })));
+	}
 }
 
-module.exports = Cat;
+module.exports = Animal;
