ec37b1c8dcee1a2f6700ed750645b21720c32372
thororen <78185467+thororen1234@users.noreply.github.com>
2023-09-08 15:20:19 -0400

    Another Fix
    I'm pretty sure this will fix the died response from the bot if a playlist is played

--- src/commands/Music/play.js
+++ src/commands/Music/play.js
@@ -233,6 +233,10 @@ class Play extends Command {
         ) {
           await player.play();
         }
+		
+		return await interaction.editReply({
+			content: `Added [${result.tracks[0].title}](${result.tracks[0].uri}) to the queue.`,
+		  });
 
       case "search":
         player.queue.add(result.tracks[0]);
