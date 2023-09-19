943f7c1af68a2e601459707b1fe77d61095149c4
codefactor-io <support@codefactor.io>
2023-09-19 20:13:14 +0000

    [CodeFactor] Apply fixes to commit 54bee68

--- src/commands/Music/playlist-create.js
+++ src/commands/Music/playlist-create.js
@@ -155,9 +155,9 @@ class PCreate extends Command {
 			case 'track': {
 				const tracks = res.playlist?.tracks.slice(0, user.premium ? 200 : 100) ?? res.tracks.slice(0, user.premium ? 200 : 100);
 				// currently broken
-				//const thumbnail = res.playlist?.tracks[0]?.thumbnail ?? res.tracks[0].thumbnail;
+				// const thumbnail = res.playlist?.tracks[0]?.thumbnail ?? res.tracks[0].thumbnail;
 				const duration = res.playlist?.duration ?? res.tracks[0].duration;
-				return await this.savePlaylist(bot, channel, user, playlistName, { tracks, /*thumbnail,*/ duration });
+				return await this.savePlaylist(bot, channel, user, playlistName, { tracks, /* thumbnail,*/ duration });
 			}
 			case 'search': {
 			// Display the options for search
@@ -185,10 +185,10 @@ class PCreate extends Command {
 
 				const tracks = res.tracks[index];
 				// currently broken
-				//const thumbnail = res.tracks[index].thumbnail;
+				// const thumbnail = res.tracks[index].thumbnail;
 				const duration = res.tracks[index].duration;
 				search.delete();
-				return await this.savePlaylist(bot, channel, user, playlistName, { tracks, /*thumbnail,*/ duration });
+				return await this.savePlaylist(bot, channel, user, playlistName, { tracks, /* thumbnail,*/ duration });
 			}
 			default:
 				return channel.error('music/p-create:NO_SONG');
@@ -214,7 +214,7 @@ class PCreate extends Command {
 				name: playlistName,
 				songs: resData.tracks,
 				timeCreated: Date.now(),
-				//thumbnail: resData.thumbnail,
+				// thumbnail: resData.thumbnail,
 				creator: user.id,
 				duration: resData.duration,
 			});
@@ -226,7 +226,7 @@ class PCreate extends Command {
 				.setDescription([
 					channel.guild.translate('music/p-create:DESC_1', { TITLE: playlistName }),
 					channel.guild.translate('music/p-create:DESC_2', { NUM: getReadableTime(parseInt(newPlaylist.duration)) }),
-					channel.guild.translate('music/p-create:DESC_3', { NUM: resData.tracks.length, TITLE: playlistName}),
+					channel.guild.translate('music/p-create:DESC_3', { NUM: resData.tracks.length, TITLE: playlistName }),
 				].join('\n'))
 				.setFooter({ text: channel.guild.translate('music/p-create:FOOTER', { ID: newPlaylist._id, NUM: newPlaylist.songs.length, PREM: (user.premium) ? '200' : '100' }) })
 				.setTimestamp();
