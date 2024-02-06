1e1dd8da7f9ca4004304c78025ec5066fd893738
Vexify4103 <47192617+Vexify4103@users.noreply.github.com>
2024-02-07 00:26:39 +0100

    Refactored pagination functionality (#553)
    * Refactored pagination functionality
    
    Refactored pagination functionality for cleaner code structure and improved readability. Added comments for better understanding of each section. Also, introduced a constant for button collector timeout for better maintainability.
    
    * Update Paginator.js

--- src/utils/Paginator.js
+++ src/utils/Paginator.js
@@ -1,65 +1,81 @@
-// variables
-const	{ ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, ComponentType } = require('discord.js');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, ComponentType } = require('discord.js');
+// Timeout for button collector in milliseconds
+const timeout = 30 * 1000;
 
-module.exports = async (bot, type, pages, userID) => {
+module.exports = async (bot, interaction, pages, userID) => {
 	let page = 0;
 
-	const row = new ActionRowBuilder()
-		.addComponents(
-			new ButtonBuilder()
-				.setCustomId('1')
-				.setLabel('⏮')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('2')
-				.setLabel('◀️')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('3')
-				.setLabel('▶️')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('4')
-				.setLabel('⏭')
-				.setStyle(ButtonStyle.Secondary),
-		);
+	// Function to create a button with specified properties
+	const createButton = (customId, label, style, disabled = false) => new ButtonBuilder().setCustomId(customId).setLabel(label).setStyle(style).setDisabled(disabled);
 
-	let curPage;
-	if (type instanceof CommandInteraction) {
-		if (!type.deferred) {
-			curPage = await type.reply({ embeds: [pages[page]], components: [row], fetchReply: true });
-		} else {
-			curPage = await type.followUp({ embeds: [pages[page]], components: [row], fetchReply: true });
-		}
+	// Create buttons for navigation
+	const first = createButton('pagefirst', '⏮', ButtonStyle.Secondary, true);
+	const prev = createButton('pageprev', '◀️', ButtonStyle.Secondary, true);
+	const pageCount = createButton('pagecount', `${page + 1}/${pages.length}`, ButtonStyle.Secondary, true);
+	const next = createButton('pagenext', '▶️', ButtonStyle.Secondary);
+	const last = createButton('pagelast', '⏭', ButtonStyle.Secondary);
+
+	// Create action row containing navigation buttons
+	const buttons = new ActionRowBuilder().addComponents([first, prev, pageCount, next, last]);
+
+	let msg;
+
+	if (interaction instanceof CommandInteraction) {
+		msg = await (interaction.deferred
+			? interaction.followUp({ embeds: [pages[page]], components: [buttons], fetchReply: true })
+			: interaction.reply({ embeds: [pages[page]], components: [buttons], fetchReply: true }));
 	} else {
-		curPage = await type.send({ embeds: [pages[page]], components: [row] });
+		msg = await interaction.send({ embeds: [pages[page]], components: [buttons] });
 	}
 
-	const buttonCollector = await curPage.createMessageComponentCollector({ componentType: ComponentType.Button });
+	const buttonCollector = await msg.createMessageComponentCollector({ componentType: ComponentType.Button, time: timeout });
 
-	// find out what emoji was reacted on to update pages
-	buttonCollector.on('collect', (i) => {
+	// Handle button interactions
+	buttonCollector.on('collect', async (i) => {
 		if (i.user.id !== userID) return;
-		switch (Number(i.customId)) {
-			case 1:
+
+		// Update page based on button custom ID
+		switch (i.customId) {
+			case 'pagefirst':
 				page = 0;
 				break;
-			case 2:
+			case 'pageprev':
 				page = page > 0 ? --page : 0;
 				break;
-			case 3:
-				page = page + 1 < pages.length ? ++page : (pages.length - 1);
+			case 'pagenext':
+				page = page + 1 < pages.length ? ++page : pages.length - 1;
 				break;
-			case 4:
+			case 'pagelast':
 				page = pages.length - 1;
 				break;
 			default:
 				break;
 		}
-		i.update({ embeds: [pages[page]] });
+
+		// Update page count label and button states
+		pageCount.setLabel(`${page + 1}/${pages.length}`);
+		first.setDisabled(page === 0);
+		prev.setDisabled(page === 0);
+		next.setDisabled(page === pages.length - 1);
+		last.setDisabled(page === pages.length - 1);
+
+		// Update message with new embed and buttons
+		await i.update({ embeds: [pages[page]], components: [buttons], ephemeral: true }).catch((error) => {
+			bot.logger.error('Error updating message:', error);
+		});
+
+		// Reset collector timer
+		buttonCollector.resetTimer();
 	});
 
-	// when timer runs out remove all reactions to show end of pageinator
-	buttonCollector.on('end', () => curPage.edit({ embeds: [pages[page]], components: [] }));
-	return curPage;
+	// Handle collector end event
+	buttonCollector.on('end', async () => {
+		// Edit interaction reply to remove buttons
+		await interaction.editReply({ embeds: [pages[page]], components: [] }).catch((error) => {
+			bot.logger.error('Error updating message:', error);
+		});
+	});
+
+	// Return the message
+	return msg;
 };
