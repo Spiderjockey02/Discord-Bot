ff88abf9bde48483a985d6384e2a5f16172f4045
Spiderjockey02 <benjamin.forey11@gmail.com>
2024-05-02 23:55:13 +0100

    Updated Logger to log connections to http

---
+++ src/utils/CONSTANTS.ts
@@ -0,0 +1,5 @@
+const CONSTANTS = {
+	ipv4Regex: /^(?:(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])\.){3}(?:\d|[1-9]\d|1\d{2}|2[0-4]\d|25[0-5])$/,
+};
+
+export default CONSTANTS;
--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,12 +1,15 @@
 // Dependencies
 import chalk from 'chalk';
 import moment from 'moment';
-import fileLog from 'simple-node-logger'
+import fileLog from 'simple-node-logger';
+import onFinished from 'on-finished';
+import CONSTANTS from './CONSTANTS';
+import { ExtendedRequest, ExtendedResponse } from 'src/types';
 const log = fileLog.createRollingFileLogger({
 	logDirectory: './src/utils/logs',
 	fileNamePattern: 'roll-<DATE>.log',
 	dateFormat: 'YYYY.MM.DD',
-})
+});
 
 type LoggerType = 'log' | 'warn' | 'error' | 'debug' | 'cmd' | 'ready'
 
@@ -40,6 +43,63 @@ export default class Logger {
 				break;
 			default:
 				break;
+		}
+	}
+
+	async connection(req: ExtendedRequest, res: ExtendedResponse) {
+		// Update request
+		await this.addEndTime(req);
+		await this.addEndTime(res);
+
+		// Get additional information
+		const	method = req.method,
+			url = req.originalUrl || req.url,
+			status = res.statusCode,
+			color = this.getStatusColor(status),
+			requester = this.getIP(req);
+
+		// How long did it take for the page to load
+		let response_time;
+		if (res._endTime && req._endTime) response_time = (res._endTime + req._endTime) - (res._startTime + req._startTime);
+
+		if (['bgCyan', 'bgGreen', 'dim'].includes(color)) {
+			this.log(`${requester} ${method} ${url} ${chalk[color](status)} - ${(response_time ?? '?')} ms`, 'log');
+		} else if (color == 'bgMagenta') {
+			this.warn(`${requester} ${method} ${url} ${chalk[color](status)} - ${(response_time ?? '?')} ms`);
+		} else {
+			this.error(`${requester} ${method} ${url} ${chalk[color](status)} - ${(response_time ?? '?')} ms`);
+		}
+	}
+
+	getStatusColor(status: number) {
+		return status >= 500 ? 'bgRed' :
+			status >= 400 ? 'bgMagenta' :
+				status >= 300 ? 'bgCyan' :
+					status >= 200 ? 'bgGreen' :
+						'dim';
+	}
+
+	async addEndTime(obj: ExtendedRequest | ExtendedResponse) {
+		await new Promise((resolve) => {
+			onFinished(obj, function() {
+				obj._endTime = new Date().getTime();
+				resolve('');
+			});
+		});
+	}
+
+	getIP(req: ExtendedRequest) {
+		if (req.headers) {
+			// Check forwarded header
+			if (req.headers['x-forwarded-for'] && CONSTANTS.ipv4Regex.test(req.headers['x-forwarded-for'][0] ?? '')) return req.headers['x-forwarded-for'];
+
+			// Remote address checks.
+			if (req.socket) {
+				if (CONSTANTS.ipv4Regex.test(req.socket.remoteAddress ?? '')) return req.socket.remoteAddress;
+				if (req.socket && CONSTANTS.ipv4Regex.test(req.socket.remoteAddress ?? '')) return req.socket.remoteAddress;
+			}
+
+			return req.ip;
 		}
 	}
 
