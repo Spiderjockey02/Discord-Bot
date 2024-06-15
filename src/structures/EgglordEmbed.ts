1dd1a49983bd821e3b24d6be6713168c2f179dae
Spiderjockey02 <benjamin.forey11@gmail.com>
2024-06-15 16:29:11 +0100

    Update translation

--- src/structures/EgglordEmbed.ts
+++ src/structures/EgglordEmbed.ts
@@ -1,4 +1,4 @@
-import { Guild, EmbedBuilder } from 'discord.js';
+import { Guild, EmbedBuilder, APIEmbedField, RestOrArray, normalizeArray } from 'discord.js';
 import EgglordClient from '../base/Egglord';
 
 /**
@@ -25,6 +25,18 @@ export default class EgglordEmbed extends EmbedBuilder {
 	// Language translator for title
 	setTitle(key: string, args?: { [key: string]: string }) {
 		this.data.title = this.client.languageManager.translate(this.guild, key, args);
+		return this;
+	}
+
+	addFields(...fields: RestOrArray<APIEmbedField>) {
+		const normalizedFields = normalizeArray(fields);
+
+		for (const field of normalizedFields) {
+			field.name = this.client.languageManager.translate(this.guild, field.name);
+		}
+
+		if (this.data.fields) this.data.fields.push(...normalizedFields);
+		else this.data.fields = normalizedFields;
 		return this;
 	}
 }
