4f848741b67cecd6a3b9d7c53e35956be638490b
Spiderjockey02 <benjamin.forey11@gmail.com>
2024-06-15 15:49:40 +0100

    Updated structures

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,10 @@
-import EgglordEmbed from './EgglordEmbed';
-import ErrorEmbed from './ErrorEmbed';
-import SuccessEmbed from './SuccessEmbed';
 import { checkNSFW, CalcLevenDist, fetchFromAPI } from './functions';
 import Logger from './Logger';
 import Paginator from './Paginator';
 import { read24hrFormat,	getReadableTime,	getTimeObject,	getTotalTime } from './timeFormatter';
 
 export {
-	EgglordEmbed, checkNSFW, CalcLevenDist, Logger,
+	checkNSFW, CalcLevenDist, Logger,
 	Paginator, read24hrFormat, getReadableTime,
 	getTimeObject, getTotalTime, fetchFromAPI,
-	ErrorEmbed, SuccessEmbed,
 };
